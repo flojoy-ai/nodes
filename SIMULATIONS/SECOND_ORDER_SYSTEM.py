0c9bf05e3dcf30fd5ccc7e5f478702ae551d76ba
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-03-27 21:06:20 -0400

    updated runtime debug info

--- SIMULATIONS/SECOND_ORDER_SYSTEM.py
+++ SIMULATIONS/SECOND_ORDER_SYSTEM.py
@@ -40,7 +40,7 @@ def SECOND_ORDER_SYSTEM(v, params):
     # v['data'] = y_primes[::-1]
 
     y_primes = np.insert(y_primes, 0, response)
-    print('-'*72+'\n'*5+f'Running iteration# {y_primes.size-1}' + ' , '+str(y_primes)+'\n'*5+'-'*72)
+    print('-'*72+'\n'*5+f'Running iteration# {y_primes.size-1}' + ' , '+str([y for y in y_primes])+'\n'*5+'-'*72)
     # We now write to memory ...
     SmallMemory().write_to_memory(node_id, memory_key, y_primes)
     # ... and return the result!
