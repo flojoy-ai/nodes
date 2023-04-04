e0a6021f972c6964c75b7c3e5d6346c9e19ca82c
Syed Mahbub <xsmahmed776@gmail.com>
2023-04-04 19:21:30 +0100

    remove unnecessary log

--- TERMINATORS/END.py
+++ TERMINATORS/END.py
@@ -3,7 +3,6 @@ from flojoy import flojoy, JobResultBuilder
 
 @flojoy
 def END(v, params):
-    print('inputs passed to END: ', v)
     return JobResultBuilder()\
         .from_inputs(v)\
         .flow_to_nodes([])\
