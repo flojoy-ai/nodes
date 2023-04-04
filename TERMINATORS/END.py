6f7b50d73ed236a71456e8179f0db00b1b2181e6
Syed Mahbub <xsmahmed776@gmail.com>
2023-04-04 18:53:57 +0100

    modify local file and object detection node

--- TERMINATORS/END.py
+++ TERMINATORS/END.py
@@ -3,6 +3,7 @@ from flojoy import flojoy, JobResultBuilder
 
 @flojoy
 def END(v, params):
+    print('inputs passed to END: ', v)
     return JobResultBuilder()\
         .from_inputs(v)\
         .flow_to_nodes([])\
