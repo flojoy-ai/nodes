8cac7bb8bdcd1b1020b8f91292b786d2ee0ac44d
angelchauffray <ac@enclose.solar>
2023-04-17 13:26:38 -0400

    update nodes

--- INSTRUMENTS/LABJACKU3.py
+++ INSTRUMENTS/LABJACKU3.py
@@ -27,21 +27,18 @@ def LABJACKU3(dc, params):                           # params {nombre de sensor}
 def LABJACKU3_MOCK (dc, params):    # params {nombre de sensor}
 
     print('running mock version of LabJackU3, number of sensor is set to 6 by default')
-    #d = u3.U3()                                      # Create an instance of U3 class d.configU3()
-    #d.configIO(FIOAnalog=255, EIOAnalog=0)           # Config the U3 for daq from temperature sensors
 
     voltages = []                                    # Declaration of variable
     temperatures = []
     temperatures_celcius = []
-    N = 6
+    N = 6                                           #Mock Number of sensors
 
     for i in range(1, N):                            #Loop on the number of sensor you are using
-        #voltage = d.getAIN(i-1)
-        voltage = 0.6
+        voltage = 0.6   # Mock Value for the measured voltage
         temperature = voltage * 100.0
         temperature_celcius = (temperature - 32) / 1.8 # Convert Voltage into temperature in Celcius
-
         voltages.append(voltage)
         temperatures.append(temperature)
         temperatures_celcius.append(temperature_celcius)  # Save measurements in lists
+
     return DataContainer(x={"a": temperatures, "b": temperatures_celcius}, y=temperatures_celcius)
