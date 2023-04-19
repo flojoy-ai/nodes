ef514f200b2b4dddc8c86111a4f8c5c7e5eeeb49
angelchauffray <ac@enclose.solar>
2023-04-19 11:44:19 -0400

    new folder categories and init files

--- INSTRUMENTS/__init__.py
+++ INSTRUMENTS/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ["KEITHLEY2400", "LABJACKU3","PHIDGET22"]
+
+from .Keithley.Keithley2400 import KEITHLEY2400
+from .Labjack.LabjackU3 import LABJACKU3
+from .Phidget.Phidget22 import PHIDGET22
+from .WEB_CAM.CAMERA import CAMERA
