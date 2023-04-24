2daaccbd061a0ec77653f98d2f2f07b37d8e9207
angelchauffray <ac@enclose.solar>
2023-04-24 14:36:58 -0400

    new node stepper

--- INSTRUMENTS/Stepper_Motor/STEPPERMOTOR.py
+++ INSTRUMENTS/Stepper_Motor/STEPPERMOTOR.py
@@ -1,4 +1,14 @@
-
+from flojoy import flojoy, DataContainer
 
 
 @flojoy
+def STEPPERMOTOR(dc, params):
+    voltage = []
+    pressions = []
+
+    return DataContainer(x={"a": voltage, "b": pressions}, y=pressions)
+
+
+@flojoy
+def STEPPERMOTOR_MOCK(dc, params):
+    return DataContainer(x={"a": voltage, "b": pressions}, y=pressions)
--- INSTRUMENTS/__init__.py
+++ INSTRUMENTS/__init__.py
@@ -2,3 +2,4 @@ from .Keithley.Keithley2400 import KEITHLEY2400
 from .Labjack.LabjackU3 import LABJACKU3
 from .Phidget.Phidget22 import PHIDGET22
 from .WEB_CAM.CAMERA import CAMERA
+from .Stepper_Motor import STEPPERMOTOR
