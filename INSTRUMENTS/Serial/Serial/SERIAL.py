1a020dbc354ac1a24fbfd71bd747ab46d602b622
dstrande <dstrande@live.ca>
2023-05-10 12:05:41 -0700

    PEP-8 and 1d condition changes.

--- INSTRUMENTS/Serial/Serial/SERIAL.py
+++ INSTRUMENTS/Serial/Serial/SERIAL.py
@@ -1,92 +1,95 @@
 from flojoy import flojoy, DataContainer
-from time import time, sleep
+from time import sleep
 import serial
 import numpy as np
 from datetime import datetime
-import plotly.express as px
+import plotly.graph_objects as go
 
 
 @flojoy
 def SERIAL(v, params):
     '''
-    Node to take simple time dependent 1d data from an Ardunio, or a similar serial device.
+    Node to take simple time dependent 1d data from an Ardunio,
+    or a similar serial device.
     For example you can record temperature following this tutorial:
 
     https://learn.adafruit.com/thermistor/using-a-thermistor
 
     with Serial.println(steinhart) as the only line printing.
 
-    It is important that the last line Arduino is returning is the data with a new line at the end (i.e. println()).
+    It is important that the last line Arduino is returning is the
+    data with a new line at the end (i.e. println()).
 
-    The other lines must be returned with print() with print(",") between each line. 
+    The other lines must be returned with print()
+    with print(",") between each line.
 
     For example:
 
     print(reading0)
     print(",")
     println(reading1)
+
+    If there is more than one column, the SELECT_ARRAY node must be
+    used after this node.
+
+    params:
+    num_readings: Number of points to record.
+    record_period: Length between two recordings in seconds.
+    BAUD_RATE: Baud rate for the serial device.
+    com_port: COM port of the serial device
+
+    num_readings * record_period is roughly the run length in seconds.
     '''
     print('parameters passed to SERIAL: ', params)
-    COM_PORT = params.get('com_port', '/dev/ttyUSB0') # The COM port to use (e.g.: Linux/Mac: '/dev/ttyUSB0' or Windows: COM1).
+    COM_PORT = params.get('com_port', '/dev/ttyUSB0')
     BAUD = int(params.get('BAUD_RATE', 9600))
-    NUM = int(params.get('num_readings', 100)) # Number of points to record. Length of recording ~= NUM * RECORD_PERIOD seconds.
-    RECORD_PERIOD = float(params.get('record_period', 1)) # Period of recordings in seconds (i.e. the time between two recordings).
-    # NUM = 5  # Debug
-
+    NUM = int(params.get('num_readings', 100))
+    RECORD_PERIOD = float(params.get('record_period', 1))
 
     ser = serial.Serial(COM_PORT, timeout=1, baudrate=BAUD)
     readings = []
     times = []
-    s = ser.readline().decode()  # The first reading is commonly empty.
-    # print(s)
+    # The first reading is commonly empty.
+    s = ser.readline().decode()
 
     for i in range(NUM):
         ts = datetime.now()
         s = ser.readline().decode()
-        if s != '':  # Some readings may be empty.
-            # print('\n', 'debug: ', s, type(s), '\n')
+        # Some readings may be empty.
+        if s != '':
             reading = s[:-2].split(',')
             readings.append(reading)
 
             ts = datetime.now()
-            # date = f'{ts.year}{ts.month:02d}{ts.day:02d}'
-            seconds = float(ts.hour * 3600 + ts.minute * 60 + ts.second + ts.microsecond / 10**6)
-            # print('Seconds: ', seconds, 'Reading: ', reading)
+            seconds = float(ts.hour * 3600 + ts.minute * 60
+                            + ts.second + ts.microsecond / 10**6)
 
             times.append(seconds)
 
             if len(times) > 0:
                 time1 = seconds - times[i]
             else:
-                time1 = 0.1  # Estimate of execution time 
+                # Estimate execution time.
+                time1 = 0.1
 
             if time1 < RECORD_PERIOD:
-                # print(f'sleeping for {RECORD_PERIOD - time1} seconds.')
                 sleep(RECORD_PERIOD - time1)
-
-    # print('')
-    # print(readings)
-    # print('')
 
     times = np.array(times)
     times -= times[0]
-    # times = times.tolist()
     readings = np.array(readings)
     readings = readings.astype('float64')
-    # y = [times, readings]
-
-    # print(type(times), type(readings))
-    # print('')
-    # print(readings[:, 0])
-    # print('')
-
-    fig = px.line(x=times, y=readings[:, 0])
-
-    return DataContainer(type='plotly', fig=fig, x=times, y=readings)
+    # If there are two or more columns return a Plotly figure.
+    if readings.ndim == 2:
+        data = go.Line(x=times, y=readings[:, 0], mode='markers')
+        fig = go.Figure(data=data)
+        return DataContainer(type='plotly', fig=fig, x=times, y=readings)
+    else:
+        return DataContainer(x=times, y=readings)
 
 
 @flojoy
-def Serial_MOCK (dc,params):
+def Serial_MOCK(dc, params):
 
     print('Running mock version of Serial')
 
--- TRANSFORMERS/SELECT_ARRAY/SELECT_ARRAY/SELECT_ARRAY.py
+++ TRANSFORMERS/SELECT_ARRAY/SELECT_ARRAY/SELECT_ARRAY.py
@@ -1,35 +1,29 @@
 from flojoy import flojoy, DataContainer
+import numpy as np
+
 
 @flojoy
 def SELECT_ARRAY(v, params):
     '''
-    Node to convert an input array with multiple columns to the selected ordered pair.
+    Node to convert an input array with multiple columns
+    to the selected ordered pair.
 
-    For example, the SERIAL node can output x=time, y1=temperature, y2=pressure.
-    This node will select one of temperautre and pressure columns to output.
+    For example, the SERIAL node can output x=time,
+    y1=temperature, y2=pressure.
+    This node will select one of temperature and pressure columns to output.
+
+    The x axis will be return unchanged.
     '''
     print('parameters passed to SELECT_ARRAY: ', params)
-    COL = int(params.get('column', 0)) # Index of the selected column.
+    # Index of the selected column.
+    COL = int(params.get('column', 0))
 
-    x = v[0].x
-    y = v[0].y[:, int(COL)]
+    # Check for numpy type. Return unchanged data if not.
+    if isinstance(v[0].y, np.ndarray):
+        x = v[0].x
+        y = v[0].y[:, int(COL)]
 
-    print('')
-    print(x)
-    print(type(x))
-    print(y)
-    print(type(y))
-    print('')
+        return DataContainer(x=x, y=y)
 
-    return DataContainer(x=x, y=y)
-
-
-# @flojoy
-# def Serial_MOCK (dc,params):
-
-#     print('Running mock version of SELECT_ARRAY')
-
-#     x = np.linspace(0, 100, 100)
-#     y = np.linspace(0, 100, 100)
-
-#     return DataContainer(x=x, y=y)
+    else:
+        return v[0]
