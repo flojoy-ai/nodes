ee40efa6d77d198d126a845ba56cb0a2bdcb17c4
Jack Parmer <jparmer09@gmail.com>
2023-05-13 17:19:04 -0400

    Update ADD.py

--- TRANSFORMERS/ARITHMETIC/ADD/ADD.py
+++ TRANSFORMERS/ARITHMETIC/ADD/ADD.py
@@ -1,11 +1,15 @@
 import numpy as np
 from flojoy import flojoy, DataContainer
 
-
 @flojoy
-def ADD(v, params):
-    """Add 2 input vectors and return the result"""
-    # print(' v in add node: ', v)
+def ADD(dc_inputs: list[DataContainer], params: dicts):
+    """ Add 2 or more numeric arrays, matrices, dataframes, or constants element-wise. 
+    When a constant is added to an array or matrix, each element in the array or 
+    matrix will be increased by the constant value. If 2 arrays or matrices of different 
+    sizes are added, the output will be the size of the larger array or matrix with 
+    only the overlapping elements changed.
+    """
+
     a = [0]
     b = [0]
 
