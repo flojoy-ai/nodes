c3b5262108f90c7889f99c1e1674d93f81eaa510
Syed Mahbub <xsmahmed776@gmail.com>
2023-05-26 23:30:03 +0100

    format code

--- EXTRACTORS/READ_CSV/READ_CSV.py
+++ EXTRACTORS/READ_CSV/READ_CSV.py
@@ -1,12 +1,13 @@
 from flojoy import flojoy, DataContainer
 import pandas as pd
 
+
 @flojoy
 def read_csv(dc, params):
-    '''
+    """
     Read a CSV file from disk or a URL, then return a dataframe.
-    '''
-    url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"    
-    file_path = params['file_path'] if 'file_path' in params else url
+    """
+    url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+    file_path = params["file_path"] if "file_path" in params else url
     df = pd.read_csv(file_path)
-    return DataContainer(type='dataframe', m=df)
+    return DataContainer(type="dataframe", m=df)
