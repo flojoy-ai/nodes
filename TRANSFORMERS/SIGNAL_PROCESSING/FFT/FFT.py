cfe64debe0b4ea144399a4fabbcef8fcfcbd1a88
Tyler Li <sh0risu179@outlook.com>
2023-06-06 14:38:32 -0400

    first steps

--- TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
+++ TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
@@ -4,30 +4,40 @@ from flojoy import flojoy, DataContainer
 
 @flojoy
 def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    """Performs an Discrete Fourier Transform on the input vector.
+    """The FFT node Performs a Discrete Fourier Transform on the input vector.
     Through the FFT algorithm, the input vector will be transformed
-    from the time domain into the frequency domain which will be an
-    ordered pair of arrays
+    from the time domain into the frequency domain which will be an ordered pair of arrays.
+    The user can specify a window type to be applied to the output with the 'window' parameter.
+    The 'real' parameter will be used to determine if the input signal is real or not.
+
+    Parameters
+    ---------
+    window: Selection of window types, optional
+    real_signal: boolean
+
+    Returns
+    -------
+
     """
     if len(dc_inputs) != 1:
         raise ValueError(
-            "Expected one input but got something else"
+            f"FFT node requires one input signal, but {len(dc_inputs)} was given!"
         )
-    window_type : str = params["window_type"] # Type of window function
-    real : bool = params["real_signal"]
+    window_type: str = params["window_type"]  # Type of window function
+    real: bool = params["real_signal"]
     signal_value = dc_inputs[0].y
-    samples = dc_inputs[0].x
+    N = dc_inputs[0].x  # Number of samples
 
    # First to avoid spectral leakage, we need to apply a
    # window function to the signal
     fourier = fft.rfft(signal_value) if real else fft.fft(signal_value)
-    if(window_type != "none"):
-        window = signal.get_window(window_type, len(signal_value))
+    if window_type:
+        window = signal.get_window(window_type, N)
         result = fourier * window
     else:
         result = fourier
 
-    return DataContainer(x=samples, y=result)
+    return DataContainer(x=N, y=result)
 
 
 
