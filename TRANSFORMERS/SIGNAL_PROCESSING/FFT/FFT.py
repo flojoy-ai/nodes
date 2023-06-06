271d03d60d605d1f4f1584ad7c4d91e986b93b7f
Tyler Li <sh0risu179@outlook.com>
2023-06-06 15:00:20 -0700

    testing

--- TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
+++ TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
@@ -7,37 +7,46 @@ def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     """The FFT node Performs a Discrete Fourier Transform on the input vector.
     Through the FFT algorithm, the input vector will be transformed
     from the time domain into the frequency domain which will be an ordered pair of arrays.
-    The user can specify a window type to be applied to the output with the 'window' parameter.
-    The 'real' parameter will be used to determine if the input signal is real or not.
 
     Parameters
     ---------
     window: Selection of window types, optional
+        the node will apply a window to the signal to avoid spectral leakage
     real_signal: boolean
+        whether the input signal is real or complex
 
     Returns
     -------
+    ordered_pair
+        x: frequency
+        y: spectrum of the signal
 
     """
     if len(dc_inputs) != 1:
         raise ValueError(
             f"FFT node requires one input signal, but {len(dc_inputs)} was given!"
         )
+    dc = dc_inputs[0]
+    if dc.type != "ordered_pair":
+        raise ValueError(
+            f"unsupported DataContainer type passed to FFT node: '{dc.type}'"
+        )
     window_type: str = params["window_type"]  # Type of window function
     real: bool = params["real_signal"]
-    signal_value = dc_inputs[0].y
-    N = dc_inputs[0].x  # Number of samples
+    signal_value = dc.y
+    x = dc.x
 
    # First to avoid spectral leakage, we need to apply a
    # window function to the signal
     fourier = fft.rfft(signal_value) if real else fft.fft(signal_value)
     if window_type:
-        window = signal.get_window(window_type, N)
+        window = signal.get_window(window_type, len(signal_value))
         result = fourier * window
     else:
         result = fourier
+    frequency = fft.fftfreq(len(x))
 
-    return DataContainer(x=N, y=result)
+    return DataContainer(x=frequency, y=result)
 
 
 
