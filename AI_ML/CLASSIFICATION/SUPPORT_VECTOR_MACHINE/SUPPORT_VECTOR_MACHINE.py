fd340aa97982999beb601a9ea21647ff060f725d
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-07 12:22:36 -0400

    wip

--- AI_ML/CLASSIFICATION/SUPPORT_VECTOR_MACHINE/SUPPORT_VECTOR_MACHINE.py
+++ AI_ML/CLASSIFICATION/SUPPORT_VECTOR_MACHINE/SUPPORT_VECTOR_MACHINE.py
@@ -1,8 +1,10 @@
 from flojoy import flojoy, DataContainer
 import pandas as pd
+import numpy as np
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from typing import cast
 
 
 @flojoy
@@ -21,16 +23,29 @@ def SUPPORT_VECTOR_MACHINE(
     """
 
     # Should input training dataset and data to classify
-    #
-    #
-    #
 
-    X = []
-    Y = []
+    x = dc_inputs[0]
+    y = dc_inputs[1]
+    input_data = dc_inputs[2]
+
+    X, Y = cast_np_array(x), cast_np_array(y)
+    data = cast_np_array(input_data)
 
     kernel = params.get("kernel", "linear")
 
     clf = svm.SVC(kernel=kernel)
+    prediction = clf.predict(input_data)
 
-    output = pd.DataFrame([])
+    output = pd.DataFrame(data)
     return DataContainer(type="dataframe", m=output)
+
+
+def cast_np_array(dc: DataContainer) -> np.ndarray:
+    if dc.type == "dataframe":
+        return cast(pd.DataFrame, dc.m).to_numpy()
+    elif dc.type == "matrix":
+        return cast(np.ndarray, dc.m)
+    else:
+        raise ValueError(
+            f"unsupported DataContainer type passed to SUPPORT_VECTOR_MACHINE node: {x.type}"
+        )
