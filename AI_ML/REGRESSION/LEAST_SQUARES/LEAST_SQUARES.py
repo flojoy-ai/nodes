91ab2f5f42a22b75228ab187b04fa72a15f36901
Youngsun Yoo <youngsun123q@gmail.com>
2023-06-09 00:27:50 +0900

    fixed docstring

--- AI_ML/REGRESSION/LEAST_SQUARES/LEAST_SQUARES.py
+++ AI_ML/REGRESSION/LEAST_SQUARES/LEAST_SQUARES.py
@@ -5,43 +5,50 @@ from flojoy import flojoy, DataContainer
 
 @flojoy
 def LEAST_SQUARES(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    """Computes the least square that minimizes the distance between
-      the inputs 'DataContainer' class, specifically the 'matrix' type
-     and the regression.
-    Args:
-        dc_inputs (list[DataContainer]: List of DataContainer objects containing
-        a coefficient matrix and dependent variable)
-        params (dict): Additional parameters for least squares method (not used in this function)
+    """Computes the least square that minimizes the distance between the 
+    inputs 'DataContainer' class, specifically the 'matrix' type and the regression.
 
-    Returns:
-        DataContainer: A 'DataContainer' class of type 'matrix'
-        that represents the weights of regression line (least squares)
-    Raises:
-        Exception: If errors occurs during least square calculation"""
 
-    x: np.ndarray = np.eye(3)
-    y: np.ndarray = np.eye(3)
-    a: np.ndarray = np.eye(3, 2)
+    Parameters
+    ----------
+    dc_inputs (list[DataContainer]: List of DataContainer objects containing
+            a coefficient matrix and dependent variable)
+            params (dict): Additional parameters for least squares method (not used in this function)
 
-    if dc_inputs.__len__ > 0:
-        if dc_inputs[0].type != "matrix":
-            raise ValueError(
-                f"unsupported DataContainer type passed to LEAST_SQUARES node: '{dc_inputs[0].type}'"
-            )
+    Returns
+    -------
+    ordered pair
+        x: input matrix (data points)
+        y: fitted line computed with returned regression weights
+    """
 
-        x = np.array(dc_inputs[0].m)
-        y = np.array(dc_inputs[1].m)
+    tmp = np.array([[1.0,2.0,3.0], [1.0,2.0,4.0], [2.0, 5.0, 1.0]])
+    return DataContainer(type="matrix", m=tmp)
+    
 
-        if x.shape[0] != y.shape[0]:
-            print("matrix dimensions do not match.")
+    # x: np.ndarray = np.eye(3)
+    # y: np.ndarray = np.eye(3)
+    # a: np.ndarray = np.eye(3, 2)
 
-        else:
-            try:
-                a = np.vstack([x, np.ones(len(x))]).T
-                res = np.linalg.lstsq(a, y, rcond=None)[0]
-            except np.linalg.LinAlgError:
-                raise ValueError("Least Square Computation failed.")
+    # if dc_inputs.__len__ > 0:
+    #     if dc_inputs[0].type != "matrix":
+    #         raise ValueError(
+    #             f"unsupported DataContainer type passed to LEAST_SQUARES node: '{dc_inputs[0].type}'"
+    #         )
 
-            return DataContainer(type="matrix", m=res)
-    else:
-        return DataContainer(type="matrix", m=np.eye(3))
+    #     x = np.array(dc_inputs[0].m)
+    #     y = np.array(dc_inputs[1].m)
+
+    #     if x.shape[0] != y.shape[0]:
+    #         print("matrix dimensions do not match.")
+
+    #     else:
+    #         try:
+    #             a = np.vstack([x, np.ones(len(x))]).T
+    #             res = np.linalg.lstsq(a, y, rcond=None)[0]
+    #         except np.linalg.LinAlgError:
+    #             raise ValueError("Least Square Computation failed.")
+
+    #         return DataContainer(type="matrix", m=res)
+    # else:
+    #     return DataContainer(type="matrix", m=np.eye(3))
