760acded9b71e64dfc732976e09f18c735f14534
Youngsun Yoo <youngsun123q@gmail.com>
2023-06-09 07:28:55 +0900

    restructured LEAST_SQUARES

--- AI_ML/REGRESSION/LEAST_SQUARES/LEAST_SQUARES.py
+++ AI_ML/REGRESSION/LEAST_SQUARES/LEAST_SQUARES.py
@@ -21,20 +21,18 @@ def LEAST_SQUARES(dc_inputs: list[DataContainer], params: dict) -> DataContainer
         x: input matrix (data points)
         y: fitted line computed with returned regression weights
     """
+    x : np.ndarray = np.eye(3)
+    y : np.ndarray = np.eye(3)
+    a : np.ndarray = np.eye(3)
     
-
-    x: np.ndarray = np.eye(3)
-    y: np.ndarray = np.eye(3)
-    a: np.ndarray = np.eye(3, 2)
-
-    if dc_inputs.__len__ > 0:
-        if dc_inputs[0].type != "matrix":
+    if (len(dc_inputs) < 0): 
             raise ValueError(
-                f"unsupported DataContainer type passed to LEAST_SQUARES node: '{dc_inputs[0].type}'"
-            )
+            f"To compute least squares, LEAST_SQUARES node requires two inputs, {len(dc_inputs)} was given!"
+    )
 
-        x = np.array(dc_inputs[0].m)
-        y = np.array(dc_inputs[1].m)
+    if (dc_inputs[0].type == "ordered_pair"):
+        x = np.array(dc_inputs[0].y)
+        y = np.array(dc_inputs[1].y)
 
         if x.shape[0] != y.shape[0]:
             print("matrix dimensions do not match.")
@@ -46,6 +44,22 @@ def LEAST_SQUARES(dc_inputs: list[DataContainer], params: dict) -> DataContainer
             except np.linalg.LinAlgError:
                 raise ValueError("Least Square Computation failed.")
 
-            return DataContainer(type="matrix", m=res)
+    elif (dc_inputs[0].typpe == "matrix"):
+        x = np.array(dc_inputs[0].m)
+        y = np.array(dc_inputs[1].m)
+
+        if x.shape[0] != y.shape[0]:
+            print("matrix dimensions do not match.")
+
+        else:
+            try:
+                a = np.vstack([x, np.ones(len(x))]).T
+                res = np.linalg.lstsq(a, y, rcond=None)[0]
+            except np.linalg.LinAlgError:
+                raise ValueError("Least Square Computation failed.")  
     else:
-        return DataContainer(type="matrix", m=np.eye(3))
+        raise ValueError(
+            f"unsupported DataContainer type passed to LEAST_SQUARES node: '{dc_inputs[0].type}'"
+        )
+
+    return DataContainer(type="matrix", m=res)
