9b1be16a4e9c9cb764440191512b77e3210d2e2a
Tyler Li <sh0risu179@outlook.com>
2023-06-08 13:44:20 -0700

    some cleanup

--- TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
+++ TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
@@ -11,7 +11,7 @@ def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
 
     Parameters
     ---------
-    window: Selection of window types, optional
+    window: Selection of window types
         the node will apply a window to the signal to avoid spectral leakage
     real_signal: boolean
         whether the input signal is real or complex
@@ -28,27 +28,34 @@ def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     """
     if len(dc_inputs) != 1:
         raise ValueError(
-            f"FFT node requires one input signal, but {len(dc_inputs)} was given!"
+            f"FFT node requires 1 input signal, but {len(dc_inputs)} was given!"
         )
     dc = dc_inputs[0]
     if dc.type != "ordered_pair":
         raise ValueError(
             f"unsupported DataContainer type passed to FFT node: '{dc.type}'"
         )
-    window_type: str = params["window_type"]  # Type of window function
+
+    window_type: str = params["window_type"]
     real: bool = params["real_signal"]
-    sample_rate: int = params["sample_rate"]
+    sample_rate: int = params["sample_rate"]  # Hz
+
+    if sample_rate <= 0:
+        raise ValueError(
+            f"Sample rate must be greater than 0"
+        )
 
     signal_value = dc.y
     x = dc.x
 
-    if window_type:
+    if window_type == "none":
+        fourier = fft.rfft(signal_value) if real else fft.fft(signal_value)
+    else:
         window = signal.get_window(window_type, len(signal_value))
         fourier = (
             fft.rfft(signal_value * window) if real else fft.fft(signal_value * window)
         )
-    else:  # no window applied
-        fourier = fft.rfft(signal_value) if real else fft.fft(signal_value)
+
     fourier = fft.fftshift(fourier)
     frequency = (
         fft.rfftfreq(x.shape[-1], 1 / sample_rate) if real else fft.fftfreq(x.shape[-1])
