9acfb6ec5803534a9813deb911c112755b7275cf
Ben Epstein <ben.epstein@wustl.edu>
2023-06-10 13:09:46 -0400

    use `pd.util.testing` until flojoy requires pandas 2.X (#95)
    pd.testing is only available in pandas 2.X. Until flojoy marks that as a dependency, use `pd.util.testing`

--- GENERATORS/SIMULATIONS/TIMESERIES/TIMESERIES.py
+++ GENERATORS/SIMULATIONS/TIMESERIES/TIMESERIES.py
@@ -12,7 +12,7 @@ def TIMESERIES(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
 
     try:
         np.random.seed(1)
-        testing.N, testing.K = 1000, 1  # rows, columns
+        pd.util.testing.N, pd.util.testing.K = 1000, 1  # rows, columns
         df = pd.util.testing.makeTimeDataFrame(freq="MS")
         return DataContainer(x=df.index.to_numpy(), y=df["A"].to_numpy())
     except Exception as e:
