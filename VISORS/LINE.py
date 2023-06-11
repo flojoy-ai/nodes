3007dd3568222f8a80cd054c58e49f42b85e3c40
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-06-11 16:49:13 -0400

    blackified

--- VISORS/LINE.py
+++ VISORS/LINE.py
@@ -6,25 +6,39 @@ import traceback
 from node_sdk.small_memory import SmallMemory
 import numpy as np
 
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+
 
 @flojoy
 def LINE(v, params):
-    referred_node = params['referred_node']
+    referred_node = params["referred_node"]
     x = v[0].y
-    if referred_node is not '':
-        referred_node_key = referred_node.split('-')[0]
+    if referred_node is not "":
+        referred_node_key = referred_node.split("-")[0]
         try:
-            job = Job.fetch(referred_node, connection=Redis(
-            host=REDIS_HOST, port=REDIS_PORT))
+            job = Job.fetch(
+                referred_node, connection=Redis(host=REDIS_HOST, port=REDIS_PORT)
+            )
             y = SmallMemory().read_memory(job.get_id(), referred_node_key)
-            print('-'*72+'\n'*5+f'{y.size-1} iterations' + f' for {referred_node_key} , '+str([i for i in y])+'\n'*5+'-'*72)
+            print(
+                "-" * 72
+                + "\n" * 5
+                + f"{y.size-1} iterations"
+                + f" for {referred_node_key} , "
+                + str([i for i in y])
+                + "\n" * 5
+                + "-" * 72
+            )
 
         except (Exception, NoSuchJobError):
             y = v[0].y if len(v) > 0 else [1, 3, 2]
             print(traceback.format_exc())
             pass
-        return JobResultBuilder().from_inputs([DataContainer(x=np.arange(0,len(y),1),y=y)]).build()
+        return (
+            JobResultBuilder()
+            .from_inputs([DataContainer(x=np.arange(0, len(y), 1), y=y)])
+            .build()
+        )
     else:
         return JobResultBuilder().from_inputs(v).build()
