b914fada2221202047cd8edb3d7384eacbcdd1c5
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-13 11:52:31 -0400

    style

--- TRANSFORMERS/CALCULUS/DIFFERENTIATE/DIFFERENTIATE.py
+++ TRANSFORMERS/CALCULUS/DIFFERENTIATE/DIFFERENTIATE.py
@@ -3,18 +3,17 @@ import numpy as np
 
 
 @flojoy
-def DIFFERENTIATE(dc_inputs : list[DataContainer], params: dict) -> DataContainer:
+def DIFFERENTIATE(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     dc_input = dc_inputs[0]
 
     if dc_input.typee != "ordered_pair":
         raise ValueError(
-           f"unsupported DataContainer type passed for DIFFERENTIATE : {dc_input.type}"
-       )
+            f"unsupported DataContainer type passed for DIFFERENTIATE : {dc_input.type}"
+        )
 
     input_x = dc_input.x
     input_y = dc_input.y
-        
+
     differentiate = np.diff(input_y) / np.diff(input_x)
 
     return DataContainer(type="ordered_pair", x=input_x, y=differentiate)
-
--- TRANSFORMERS/CALCULUS/INTEGRATE/INTEGRATE.py
+++ TRANSFORMERS/CALCULUS/INTEGRATE/INTEGRATE.py
@@ -1,19 +1,18 @@
 from flojoy import flojoy, DataContainer
 import numpy as np
 
+
 @flojoy
-def INTEGRATE(dc_inputs : list[DataContainer], params : dict) -> DataContainer:
+def INTEGRATE(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     dc_input = dc_inputs[0]
 
     if dc_input.type != "ordered_pair":
         raise ValueError(
-           f"unsupported DataContainer type passed for INTEGRATE : {dc_input.type}"
-       )
+            f"unsupported DataContainer type passed for INTEGRATE : {dc_input.type}"
+        )
 
     x = dc_input.x
     y = dc_input.y
     integrate = np.trapz(x, y)
 
     return DataContainer(type="ordered_pair", x=x, y=integrate)
-
-
