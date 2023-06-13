e60d4d3944b99bda5ed0d10ee3c79a53e550721e
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-13 10:57:40 -0400

    working on the nodes

--- TRANSFORMERS/CALCULUS/DIFFERENTIATE/DIFFERENTIATE.py
+++ TRANSFORMERS/CALCULUS/DIFFERENTIATE/DIFFERENTIATE.py
@@ -1,25 +1,50 @@
-from sympy import symbols, diff, parse_expr
+# from sympy import symbols, diff, parse_expr
 from flojoy import flojoy, DataContainer
+import numpy as np
 
 
 @flojoy
 def DIFFERENTIATE(dc_inputs : list[DataContainer], params: dict) -> DataContainer:
+    dc_input = dc_inputs[0]
 
-    if params["derivative"] != len(params["respective"]):
+    if dc_input == "ordered_triple":
+        input_x = dc_input.x
+        input_y = dc_input.y
+        if type(input) == "list":
+            differentiate = np.diff(input_y) / np.diff(input_x)
+        else:
+            differentiate = np.diff(input_y) / input_x
+
+        return DataContainer(type="ordered_pair", x=input_x, y=input_y, z=differentiate)
+
+    else:
         raise ValueError(
-            "Number of derivative that has to be performed does not match the respective variables"
-        )
+           f"unsupported DataContainer type passed for DIFFERENTIATE : {dc_input.type}"
+       )
+
+
+
+
+
+
+
+
+
+    # if params["derivative"] != len(params["respective"]):
+    #     raise ValueError(
+    #         "Number of derivative that has to be performed does not match the respective variables"
+    #     )
     
-    equation_str = params["equation"]
+    # equation_str = params["equation"]
 
-    try:
-        equation = parse_expr(equation_str)
+    # try:
+    #     equation = parse_expr(equation_str)
 
-        for resp in params["respective"]:
-            derivative = diff(equation, symbols(resp))
-            equation = derivative
+    #     for resp in params["respective"]:
+    #         derivative = diff(equation, symbols(resp))
+    #         equation = derivative
 
-        return DataContainer(type="ordered_pair", x=equation_str, y=derivative)
+    #     return DataContainer(type="ordered_pair", x=equation_str, y=derivative)
 
-    except Exception as e:
-        print("Error:", e)
+    # except Exception as e:
+    #     print("Error:", e)
