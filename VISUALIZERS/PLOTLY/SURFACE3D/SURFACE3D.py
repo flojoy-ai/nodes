9ff04bc36b0d71949b257f038872205bf90a4487
Syed Mahbub <86223939+smahmed776@users.noreply.github.com>
2023-06-16 00:00:40 +0100

    update scatter3d and surface3d nodes (#120)

--- VISUALIZERS/PLOTLY/SURFACE3D/SURFACE3D.py
+++ VISUALIZERS/PLOTLY/SURFACE3D/SURFACE3D.py
@@ -1,20 +1,41 @@
 import plotly.graph_objects as go
 from flojoy import DataContainer, flojoy
 from nodes.VISUALIZERS.template import plot_layout
+import pandas as pd
 
 
 @flojoy
 def SURFACE3D(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
+    """The SURFACE3D node creates a Plotly 3D Surface visualization for a given input data container.
+
+    Parameters:
+    -----------
+    None
+
+    Supported DC types:
+    -------------------
+    `ordered_triple`, `dataframe`
+    """
     dc_input: DataContainer = dc_inputs[0]
     node_name = __name__.split(".")[-1]
-    if dc_input.type == "ordered_triple":
-        x = dc_input.x
-        y = dc_input.y
-        z = dc_input.z
-    else:
-        raise ValueError(
-            f"unsupported DataContainer type passed for {node_name}: {dc_input.type}"
-        )
     layout = plot_layout(title=node_name)
-    fig = go.Figure(data=go.Surface(x=x, y=y, z=z, mode="markers"), layout=layout)
-    return DataContainer(type="plotly", fig=fig, x=x, y=y, z=z)
+    fig = go.Figure(layout=layout)
+    match dc_input.type:
+        case "ordered_triple":
+            x = dc_input.x
+            y = dc_input.y
+            z = dc_input.z
+            fig = go.Figure(
+                data=[
+                    go.Surface(x=x, y=y, z=z),
+                ],
+                layout=layout,
+            )
+        case "dataframe":
+            df = pd.DataFrame(dc_input.m)
+            fig = go.Figure(data=go.Surface(z=df.values), layout=layout)
+        case _:
+            raise ValueError(
+                f"unsupported DataContainer type passed for {node_name}: {dc_input.type}"
+            )
+    return DataContainer(type="plotly", fig=fig)
