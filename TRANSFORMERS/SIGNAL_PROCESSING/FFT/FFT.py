ce403f5e6e4d04caaf7dd2764324b8f206e33471
Tyler Li <sh0risu179@outlook.com>
2023-06-16 11:31:02 -0700

    removed file changes from another pr

--- TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
+++ TRANSFORMERS/SIGNAL_PROCESSING/FFT/FFT.py
@@ -5,7 +5,7 @@ from flojoy import flojoy, DataContainer
 
 @flojoy
 def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    """The FFT node performs a Discrete Fourier Transform on the input vector.
+    """The FFT node Performs a Discrete Fourier Transform on the input vector.
     Through the FFT algorithm, the input vector will be transformed
     from the time domain into the frequency domain which will be an ordered pair of arrays.
 
@@ -16,9 +16,8 @@ def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     real_signal: boolean
         whether the input signal is real or complex
     sample_rate: int
-        the sample rate of the signal, defaults to 1
-    display: boolean
-        whether the output would be graphed, set to false for pure data, true for data that's more suitable to be graphed
+        the sample rate of the real signal, if the input is complex, it will default to 1
+        regardless of the input
 
     Returns
     -------
@@ -40,42 +39,26 @@ def FFT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     window_type: str = params["window_type"]
     real: bool = params["real_signal"]
     sample_rate: int = params["sample_rate"]  # Hz
-    display: bool = params["display"]
 
     if sample_rate <= 0:
         raise ValueError(f"Sample rate must be greater than 0")
 
     signal_value = dc.y
     x = dc.x
-    sample_spacing = 1.0 / sample_rate
 
-    if display:
-        # x-axis
-        frequency = (
-            fft.rfftfreq(x.shape[-1], sample_spacing)
-            if real
-            else fft.fftfreq(x.shape[-1], sample_spacing)
-        )
-        frequency = fft.fftshift(frequency)
-
-        # y-axis
-        if window_type == "none":
-            fourier = fft.rfft(signal_value) if real else fft.fft(signal_value)
-        else:
-            window = signal.get_window(window_type, len(signal_value))
-            fourier = (
-                fft.rfft(signal_value * window)
-                if real
-                else fft.fft(signal_value * window)
-            )
-        fourier = fft.fftshift(fourier)
-        fourier = abs(fourier)
-    else:  # used for processing
+    if window_type == "none":
         fourier = fft.rfft(signal_value) if real else fft.fft(signal_value)
-        frequency = x
+    else:
+        window = signal.get_window(window_type, len(signal_value))
+        fourier = (
+            fft.rfft(signal_value * window) if real else fft.fft(signal_value * window)
+        )
 
-    return (
-        DataContainer(x=frequency, y=fourier)
-        if display
-        else DataContainer(x=frequency, y={"real": fourier.real, "imag": fourier.imag})
+    fourier = fft.fftshift(fourier)
+    frequency = (
+        fft.rfftfreq(x.shape[-1], 1 / sample_rate) if real else fft.fftfreq(x.shape[-1])
     )
+    frequency = fft.fftshift(frequency)
+
+    result = abs(fourier)
+    return DataContainer(x=frequency, y=result)
