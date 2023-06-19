664583463603d3760631eea5b173af7e4c654cfd
angelchauffray <ac@enclose.solar>
2023-06-19 15:21:08 -0400

    modified function and remove mock

--- INSTRUMENTS/RASPBERRY_PI/CPU_TEMP_RAM.py
+++ INSTRUMENTS/RASPBERRY_PI/CPU_TEMP_RAM.py
@@ -1,6 +1,4 @@
 from flojoy import flojoy, DataContainer
-
-# import psutil (We need to download the library to get info about sensor)
 import psutil
 
 
@@ -14,6 +12,9 @@ def CPU_TEMP_RAM(dc, params):
     ------------
     None
     """
+    memory_info = psutil.virtual_memory()
+    memory_free = memory_info.available
+    memory_free_go = memory_free / (1024**3)
 
     temperatures = psutil.sensors_temperatures()
 
@@ -24,26 +25,9 @@ def CPU_TEMP_RAM(dc, params):
                 cpu_key = key
                 break
 
-    temp1 = psutil.sensors_temperatures()["system76_acpi"][0].current
-    temp2 = temperatures[cpu_key][0].current
+    temp = temperatures[cpu_key][0].current
 
-    memory_info = psutil.virtual_memory()
-    memory_free = memory_info.available
-    memory_free_go = memory_free / (1024**3)
+    cpu_data = [temp, memory_free_go]
+    data_description = ["CPU Temp (°C)", "RAM Available (Go)"]
 
-    Temp = [temp1, memory_free_go]
-    x_plot = ["CPU Temp (°C)", "RAM Available (Go)"]
-
-    return DataContainer(type="ordered_pair", x=x_plot, y=Temp)
-
-
-@flojoy
-def CPU_TEMP_RAM_MOCK(dc, params):
-    """
-    Mock Version of the RaspberryPi node, return mock values
-    """
-
-    cpu_temp = 35
-    raspberry_memory = 0
-
-    return DataContainer(type="ordered_pair", x=cpu_temp, y=raspberry_memory)
+    return DataContainer(type="ordered_pair", x=data_description, y=cpu_data)
