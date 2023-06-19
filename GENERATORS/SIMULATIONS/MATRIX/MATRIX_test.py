9eb65cbe05bd49aff9157b768ae53f05d2c8c507
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-19 13:20:52 -0400

    style

--- GENERATORS/SIMULATIONS/MATRIX/MATRIX_test.py
+++ GENERATORS/SIMULATIONS/MATRIX/MATRIX_test.py
@@ -3,6 +3,7 @@ import numpy as np
 from functools import wraps
 from unittest.mock import patch
 
+
 def mock_flojoy_decorator(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -10,15 +11,16 @@ def mock_flojoy_decorator(f):
 
     return decorated_function
 
+
 patch("flojoy.flojoy", mock_flojoy_decorator).start()
 
 import MATRIX
 
+
 def test_MATRIX():
     # create the two matrices
-    m1 = MATRIX.MATRIX([], {"row": 3, "column" : 4})
-    m2 = MATRIX.MATRIX([], {"row": 3, "column" : 4})
+    m1 = MATRIX.MATRIX([], {"row": 3, "column": 4})
+    m2 = MATRIX.MATRIX([], {"row": 3, "column": 4})
 
     # Check if they are equal
-    assert (np.array_equal(m1, m2))
-        
+    assert np.array_equal(m1, m2)
--- VISUALIZERS/DATA_STRUCTURE/MATRIX_VIEW/MATRIX_VIEW_test.py
+++ VISUALIZERS/DATA_STRUCTURE/MATRIX_VIEW/MATRIX_VIEW_test.py
@@ -3,6 +3,7 @@ import numpy as np
 from functools import wraps
 from unittest.mock import patch
 
+
 def mock_flojoy_decorator(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -10,19 +11,19 @@ def mock_flojoy_decorator(f):
 
     return decorated_function
 
+
 patch("flojoy.flojoy", mock_flojoy_decorator).start()
 
 import MATRIX_VIEW
 from PYTHON.nodes.GENERATORS.SIMULATIONS.MATRIX.MATRIX import MATRIX
 
+
 def test_MATRIX_VIEW():
     try:
         # generate a MATRIX that has different number of rows and columns
-        m1 = MATRIX([], {"row": 3, "column" : 4})
-        
+        m1 = MATRIX([], {"row": 3, "column": 4})
+
         # run MATRIX_VIEW function
-        MATRIX_VIEW.MATRIX_VIEW([m1],{})
+        MATRIX_VIEW.MATRIX_VIEW([m1], {})
     except:
         raise AssertionError("Unable visualize the matrix")
-
-
