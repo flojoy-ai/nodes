98816d404ff8a705cd76b70f6eacdc3d9ad9ef83
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-19 14:02:35 -0400

    docstring

--- EXTRACTORS/FILE/READ_S3/READ_S3.py
+++ EXTRACTORS/FILE/READ_S3/READ_S3.py
@@ -9,6 +9,24 @@ from flojoy import flojoy, DataContainer
 
 @flojoy
 def READ_S3(dc_inputs: list[DataContainer], params: dict[str, str]):
+    """
+    The READ_S3 node takes S3_key name, S3 bucket name, and file name as input,
+    and extract the file from the specified bucket using the S3_key that they saved.
+
+    Parameters
+    ----------
+    s3_name:
+        name of the key that the user used to save access and secret access key
+    bucket_name:
+        AWS S3 bucket name that they are trying to access
+    file_name:
+        name of the file that they want to extract
+
+    Returns
+    ------
+    DataContainer:
+        type 'dataframe', m
+    """
     try:
         name = params["s3_name"]
 
