8812b0879af008374635462a2ef7436f022a222e
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-06-19 12:18:41 -0400

    Calculus nodes (#104)
    ---------
    
    Co-authored-by: JeffDotPng <zhangjeff2014@gmail.com>

---
+++ TRANSFORMERS/CALCULUS/INTEGRATE/INTEGRATE.py
@@ -0,0 +1,51 @@
+from flojoy import flojoy, DataContainer
+import numpy as np
+
+
+def trapz(x, y):
+    m = [0] * len(x)
+    trapezium = (1 / 2) * (x[1] - x[0]) * (y[1] + y[0])
+    m[1] = trapezium
+
+    for i in range(2, len(x)):
+        trapezium = (1 / 2) * (x[i] - x[i - 1]) * (y[i] + y[i - 1])
+        m[i] = m[i - 1] + trapezium
+
+    return m
+
+
+@flojoy
+def INTEGRATE(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
+    """
+    The INTEGRATE node takes two lists as input and integrates it using the composite
+    trapezoidal rule.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    DataContainer:
+        type 'ordered_pair', x, y
+    """
+    dc_input = dc_inputs[0]
+
+    input_x = dc_input.x
+    input_y = dc_input.y
+
+    if dc_input.type != "ordered_pair":
+        raise ValueError(
+            f"unsupported DataContainer type passed for INTEGRATE : {dc_input.type}"
+        )
+
+    if type(input_x) != np.ndarray:
+        raise ValueError(f"Invalid type for x:{type(input_x)}")
+    elif type(input_y) != np.ndarray:
+        raise ValueError(f"Invalid type for y:{type(input_y)}")
+    elif len(input_x) != len(input_y):
+        raise ValueError(f"Invalid inputs, x:{input_x} y:{input_y}")
+
+    integrate = trapz(input_x, input_y)
+
+    return DataContainer(type="ordered_pair", x=input_x, y=integrate)
