75cc5d6156bc9ce58a68adfa3de0c3135184ce46
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-06-21 08:33:12 -0400

    black formatter

--- AI_ML/NLP/TEXT_SUMMARIZATION/BART_LARGE_CNN/BART_LARGE_CNN.py
+++ AI_ML/NLP/TEXT_SUMMARIZATION/BART_LARGE_CNN/BART_LARGE_CNN.py
@@ -4,8 +4,6 @@ from transformers import BartTokenizer, BartForConditionalGeneration
 import pandas as pd
 
 
-
-
 @flojoy
 def BART_LARGE_CNN(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     """The BART_LARGE_CNN node takes an input dataframe with multiple rows and a single "text" column,
@@ -28,27 +26,57 @@ def BART_LARGE_CNN(dc_inputs: list[DataContainer], params: dict) -> DataContaine
         )
     input_df = dc_inputs[0].m
 
-    assert len(input_df.columns.tolist()) == 1, "Can only take a single-column dataframe as input"
+    assert (
+        len(input_df.columns.tolist()) == 1
+    ), "Can only take a single-column dataframe as input"
 
     # Load the pre-trained BART model
-    model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn', revision="3d22493")
-    tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn', revision="3d22493")
+    model = BartForConditionalGeneration.from_pretrained(
+        "facebook/bart-large-cnn", revision="3d22493"
+    )
+    tokenizer = BartTokenizer.from_pretrained(
+        "facebook/bart-large-cnn", revision="3d22493"
+    )
 
     def _chunk_text(text):
-        inputs_no_trunc = tokenizer(text, max_length=None, return_tensors='pt', truncation=False)
+        inputs_no_trunc = tokenizer(
+            text, max_length=None, return_tensors="pt", truncation=False
+        )
         chunks = []
-        for i in range(0, len(inputs_no_trunc['input_ids'][0]), tokenizer.model_max_length):
-            chunk = inputs_no_trunc['input_ids'][0][i:i + tokenizer.model_max_length]
+        for i in range(
+            0, len(inputs_no_trunc["input_ids"][0]), tokenizer.model_max_length
+        ):
+            chunk = inputs_no_trunc["input_ids"][0][i : i + tokenizer.model_max_length]
             chunks.append(torch.unsqueeze(chunk, 0))
         return chunks
 
     def _summarize_text(text):
         chunks = _chunk_text(text)
-        summary_ids = [model.generate(chunk, num_beams=4, max_length=tokenizer.model_max_length // 2, early_stopping=True) for chunk in chunks]
-        summaries = ['\n'.join([tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in id]) for id in summary_ids]
-        return '\n'.join(summaries)
-    
+        summary_ids = [
+            model.generate(
+                chunk,
+                num_beams=4,
+                max_length=tokenizer.model_max_length // 2,
+                early_stopping=True,
+            )
+            for chunk in chunks
+        ]
+        summaries = [
+            "\n".join(
+                [
+                    tokenizer.decode(
+                        g, skip_special_tokens=True, clean_up_tokenization_spaces=False
+                    )
+                    for g in id
+                ]
+            )
+            for id in summary_ids
+        ]
+        return "\n".join(summaries)
+
     column = input_df.columns[0]
-    
-    output_df = pd.DataFrame(input_df[column].apply(_summarize_text).rename("summary_text"))
+
+    output_df = pd.DataFrame(
+        input_df[column].apply(_summarize_text).rename("summary_text")
+    )
     return DataContainer(type="dataframe", m=output_df)
