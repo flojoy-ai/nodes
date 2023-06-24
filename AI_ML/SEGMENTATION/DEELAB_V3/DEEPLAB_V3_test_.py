72fc5b00f3cce1d3d912318f57257307b1705560
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-06-24 09:33:56 -0400

    black formatter

--- AI_ML/SEGMENTATION/DEELAB_V3/DEEPLAB_V3_test_.py
+++ AI_ML/SEGMENTATION/DEELAB_V3/DEEPLAB_V3_test_.py
@@ -17,9 +17,7 @@ def mock_flojoy_decorator():
 
 @pytest.fixture
 def obama_image_array_rgb():
-    _image_path = (
-        f"{os.path.dirname(os.path.realpath(__file__))}/obama_image.jpg"
-    )
+    _image_path = f"{os.path.dirname(os.path.realpath(__file__))}/obama_image.jpg"
     image = Image.open(_image_path).convert("RGB")
     return np.array(image)
 
