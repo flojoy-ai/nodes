383a669b22157cc4d9eb00fae08906bacffdb1a0
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-26 16:49:15 -0400

    New node API example

--- GENERATORS/SIMULATIONS/SINE/SINE.py
+++ GENERATORS/SIMULATIONS/SINE/SINE.py
@@ -1,31 +1,31 @@
 import numpy as np
-from flojoy import flojoy, DataContainer
+from flojoy import flojoy, OrderedPair
 from scipy import signal
+from typing import Literal
 
 
 @flojoy
-def SINE(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    valid_waveforms = ["sine", "square", "triangle", "sawtooth"]
-    x = None
-    if dc_inputs.__len__() > 0:
-        x = dc_inputs[0].y
+def SINE(
+    default: OrderedPair,
+    amplitude: float = 1,
+    frequency: float = 1,
+    offset: float = 0,
+    phase: float = 0,
+    waveform: Literal["sine", "square", "triangle", "sawtooth"] = "sine",
+) -> OrderedPair:
+    x = default.y
 
-    waveform = params["waveform"]
-    A = params["amplitude"]
-    F = params["frequency"]
-    Y0 = params["offset"]
-    PHASE = params["phase"]
-    if waveform not in valid_waveforms:
-        waveform = valid_waveforms[0]
-        print("invalid waveform passed as param, using default:", waveform)
+    A = amplitude
+    F = frequency
+    Y0 = offset
 
     if waveform == "sine":
-        y = Y0 + A * np.sin(2 * np.pi * F * x + PHASE)
+        y = Y0 + A * np.sin(2 * np.pi * F * x + phase)
     elif waveform == "square":
-        y = Y0 + A * signal.square(2 * np.pi * F * x / 10 + PHASE)
+        y = Y0 + A * signal.square(2 * np.pi * F * x / 10 + phase)
     elif waveform == "triangle":
-        y = Y0 + A * signal.sawtooth(2 * np.pi * F * x / 10 + PHASE, 0.5)
+        y = Y0 + A * signal.sawtooth(2 * np.pi * F * x / 10 + phase, 0.5)
     elif waveform == "sawtooth":
-        y = Y0 + A * signal.sawtooth(2 * np.pi * F / 10 * x + PHASE)
+        y = Y0 + A * signal.sawtooth(2 * np.pi * F / 10 * x + phase)
 
-    return DataContainer(x=x, y=y)
+    return OrderedPair(x=x, y=y)
