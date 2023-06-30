b3f1593a0e33aa8ded49c4644a9a5591c297a725
Tyler Li <sh0risu179@outlook.com>
2023-06-29 19:33:49 -0700

    changed random node

--- GENERATORS/SIMULATIONS/RAND/RAND.py
+++ GENERATORS/SIMULATIONS/RAND/RAND.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 from flojoy import flojoy, DataContainer
 import traceback
@@ -5,26 +6,41 @@ import traceback
 
 @flojoy
 def RAND(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    x = None
-    my_generator = np.random.default_rng()
+    """ The RAND node generates a random number or a list of numbers with
+    various different distributions
+    Parameters
+    ----------
+    size: The number of random numbers
+    distribution: the distribution over the random samples
+
+    Returns:
+    -------
+    scalar
+        c: The list of random numbers
+
+    """
+    size: int = params["size"]
+    distribution: str = params["distribtuion"]
+    seed: int = random.randint(-10000, 10000)
+    my_generator = np.random.default_rng(seed)
     if len(dc_inputs) > 0:
         x = dc_inputs[0].y
-        y = my_generator.normal(size=len(x))
+        y = getattr(my_generator, distribution)(size=len(x))
     else:
-        y = my_generator.normal(size=1000)
+        y = getattr(my_generator, distribution)(size=size)
 
-    return DataContainer(x=x, y=y)
+    return DataContainer(type='scalar', c=y)
 
 
-@flojoy
-def RAND_MOCK(dc_inputs, params):
-    print("running mock version of rand")
-    x = None
-    if len(dc_inputs) > 0:
-        x = dc_inputs[0].y
-        y = x
-    else:
-        y = np.full(
-            1000, 1000
-        )  # for reproducibility returning an array with constant values
-    return DataContainer(x=x, y=y)
+# @flojoy
+# def RAND_MOCK(dc_inputs, params):
+#     print("running mock version of rand")
+#     x = None
+#     if len(dc_inputs) > 0:
+#         x = dc_inputs[0].y
+#         y = x
+#     else:
+#         y = np.full(
+#             1000, 1000
+#         )  # for reproducibility returning an array with constant values
+#     return DataContainer(x=x, y=y)
