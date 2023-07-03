e240e80bf7799a0975a190a93a918cd345b2c369
dstrande <dstrande@live.ca>
2023-07-03 10:02:57 -0700

    initial

--- TRANSFORMERS/ARITHMETIC/ABS/ABS.py
+++ TRANSFORMERS/ARITHMETIC/ABS/ABS.py
@@ -4,5 +4,26 @@ from flojoy import flojoy, DataContainer
 
 @flojoy
 def ABS(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    """Returns abolute value"""
-    return DataContainer(x=dc_inputs[0].y, y=np.abs(dc_inputs[0].y))
+    """The ABS node returns the absolute value for the input.
+    The node can handle scalar and ordered pair inputs currently
+
+    TODO - reconcile.py so matrix and dataframe types can be handled.
+
+    Parameters
+    ----------
+    None
+
+    Returns:
+    --------
+    DataContainer:
+        type 'ordered pair' if 'ordered pair' is input.
+
+        type 'scalar' if 'scalar' is input.
+    """
+
+    match dc_inputs[0].type:
+        case "scalar":
+            return DataContainer(type='scalar', c=np.abs(dc_inputs[0].c))
+
+        case "ordered_pair":
+            return DataContainer(x=dc_inputs[0].x, y=np.abs(dc_inputs[0].y))
--- TRANSFORMERS/ARITHMETIC/SUBTRACT/SUBTRACT.py
+++ TRANSFORMERS/ARITHMETIC/SUBTRACT/SUBTRACT.py
@@ -4,16 +4,55 @@ from flojoy import flojoy, DataContainer
 
 @flojoy
 def SUBTRACT(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
-    """Subtract 2 input vectors and return the result"""
+    """The SUBTRACT node subtracts the values from the two inputs.
+    The node can handle scalar and ordered pair inputs currently
+
+    TODO - reconcile.py so matrix and dataframe types can be handled.
+
+    Parameters
+    ----------
+    None
+
+    Returns:
+    --------
+    DataContainer:
+        type 'ordered pair' if one 'ordered pair' is input.
+
+        type 'scalar' if two 'scalars' are input.
+    """
 
     if len(dc_inputs) < 2:
         raise ValueError(
-            f"To substract the values, SUBSTRACT node requires two inputs, {len(dc_inputs)} was given!"
+            f"To subtract the values, SUBTRACT node requires two inputs, {len(dc_inputs)} was given!"
         )
-    a = dc_inputs[0].y
-    b = dc_inputs[1].y
+    match [dc_inputs[0].type, dc_inputs[1].type]:
+        case ["scalar", "ordered_pair"]:
+            raise ValueError(
+                "The 'scalar' type should be connect to the 'y' input."
+            )
 
-    x = dc_inputs[0].x
-    y = np.subtract(a, b)
+        case ["ordered_pair", "ordered_pair"]:
+            a = dc_inputs[0].y
+            b = dc_inputs[1].y
 
-    return DataContainer(x=x, y=y)
+            x = dc_inputs[0].x
+            y = np.subtract(a, b)
+
+            return DataContainer(x=x, y=y)
+
+        case ["ordered_pair", "scalar"]:
+            a = dc_inputs[0].y
+            b = dc_inputs[1].c
+
+            x = dc_inputs[0].x
+            y = np.subtract(a, b)
+
+            return DataContainer(x=x, y=y)
+
+        case ["scalar", "scalar"]:
+            a = dc_inputs[0].c
+            b = dc_inputs[1].c
+
+            c = np.subtract(a, b)
+
+            return DataContainer(type="scalar", c=c)
