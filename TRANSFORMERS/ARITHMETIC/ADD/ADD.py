5f514202395287ac7686827928904d2f05e0fe36
JeffDotPng <zhangjeff2014@gmail.com>
2023-07-04 13:18:01 -0400

    Arbitrary inputs proof of concept

--- TRANSFORMERS/ARITHMETIC/ADD/ADD.py
+++ TRANSFORMERS/ARITHMETIC/ADD/ADD.py
@@ -3,7 +3,7 @@ from flojoy import flojoy, OrderedPair
 
 
 @flojoy(node_type="ARITHMETIC")
-def ADD(a: OrderedPair, b: OrderedPair) -> OrderedPair:
+def ADD(a: OrderedPair, b: list[OrderedPair]) -> OrderedPair:
     """Add 2 or more numeric arrays, matrices, dataframes, or constants element-wise.
     When a constant is added to an array or matrix, each element in the array or
     matrix will be increased by the constant value. If 2 arrays or matrices of different
@@ -14,9 +14,8 @@ def ADD(a: OrderedPair, b: OrderedPair) -> OrderedPair:
     # TODO: Support other data types
     x = a.x
 
-    u = a.y
-    v = b.y
-
-    y = np.add(u, v)
+    y = a.y
+    for v in b:
+        y = np.add(y, v.y)
 
     return OrderedPair(x=x, y=y)
