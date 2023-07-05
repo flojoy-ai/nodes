97ceb0a19a1664a29b605f7d3bc6ac63a9834f89
Hristo <sandevh@gmail.com>
2023-07-05 11:47:25 -0400

    style and small fixes

--- INSTRUMENTS/CONTROLLER/GAMEPAD/GAMEPAD.py
+++ INSTRUMENTS/CONTROLLER/GAMEPAD/GAMEPAD.py
@@ -1,6 +1,7 @@
 import hid
-import numpy as np 
+import numpy as np
 from flojoy import flojoy, DataContainer
+
 
 @flojoy
 def GAMEPAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
@@ -11,37 +12,36 @@ def GAMEPAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     - the y value is a numpy array of booleans indicating which buttons are pressed
     """
 
-    gamepad_device = None 
+    gamepad_device = None
     clicked_buttons = [False] * 12
 
     for device in hid.enumerate():
-        if "gamepad" in device['product_string'].lower():
+        if "gamepad" in device["product_string"].lower():
             gamepad_device = device
 
     if gamepad_device is None:
         print("ERROR: No gamepad found")
-        return DataContainer(type='ordered_pair', x=0, y=np.array(clicked_buttons))
+        return DataContainer(type="ordered_pair", x=0, y=np.array(clicked_buttons))
 
     gamepad = hid.device()
     gamepad.open(gamepad_device["vendor_id"], gamepad_device["product_id"])
 
     report = gamepad.read(64)
 
-    '''
+    """
     check backside buttons
-    '''
+    """
     if report[6] & 1:
         clicked_buttons[0] = True
-    if report[6] & 2: 
+    if report[6] & 2:
         clicked_buttons[1] = True
-        
 
-    '''
+    """
     check rightside buttons
-    '''
+    """
     if report[5] & 0b01000000:
         clicked_buttons[2] = True
-    
+
     if report[5] & 0b00100000:
         clicked_buttons[3] = True
 
@@ -51,9 +51,9 @@ def GAMEPAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     if report[5] & 0b00010000:
         clicked_buttons[5] = True
 
-    '''
+    """
     check leftside buttons
-    '''
+    """
     if report[4] & 0b10000000:
         clicked_buttons[6] = True
 
@@ -66,13 +66,13 @@ def GAMEPAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     if report[3] == 0b00000000:
         clicked_buttons[9] = True
 
-    '''
+    """
     check center buttons
-    '''
+    """
     if report[6] & 0b00010000:
         clicked_buttons[10] = True
-    
+
     if report[6] & 0b00100000:
         clicked_buttons[11] = True
 
-    return DataContainer(type='ordered_pair', x=11, y=np.array(clicked_buttons))
+    return DataContainer(type="ordered_pair", x=11, y=np.array(clicked_buttons))
--- INSTRUMENTS/LED/LED_MATRIX_WS2812B/LED_MATRIX_WS2812B.py
+++ INSTRUMENTS/LED/LED_MATRIX_WS2812B/LED_MATRIX_WS2812B.py
@@ -1,10 +1,11 @@
 from flojoy import flojoy, DataContainer
 import serial
 
+
 @flojoy
 def LED_MATRIX_WS2812B(dc_inputs: list[DataContainer], params: dict) -> dict:
     """
-    The LED_MATRIX_WS2812B node takes an image as an input and sends signals to the LED Matrix to light up specific 
+    The LED_MATRIX_WS2812B node takes an image as an input and sends signals to the LED Matrix to light up specific
     LEDs accoring to the image input
 
     Parameters:
@@ -13,13 +14,15 @@ def LED_MATRIX_WS2812B(dc_inputs: list[DataContainer], params: dict) -> dict:
     - height: the height of the LED Matrix
     """
     input = dc_inputs[0]
-    if input.type != 'image':
-        raise ValueError(f"unsupported DataContainer type passed for LED_MATRIX_WS2812B: {input.type}")
+    if input.type != "image":
+        raise ValueError(
+            f"unsupported DataContainer type passed for LED_MATRIX_WS2812B: {input.type}"
+        )
     port = params.get("port")
-    width = params.get("width") # width of the LED matrix, not the image
+    width = params.get("width")  # width of the LED matrix, not the image
     height = params.get("height")
 
-    arduino = serial.Serial(port, 115200) 
+    arduino = serial.Serial(port, 115200)
 
     cmd = ""
     # Use the function to light up the LED
@@ -29,7 +32,7 @@ def LED_MATRIX_WS2812B(dc_inputs: list[DataContainer], params: dict) -> dict:
             # So, for a 5x5 LED, we have 25 LEDs in consecutive order, and after the 5th LED
             # on the first row, the 6th LED starts at the end of the second row, then after the 10th LED,
             # the 11th LED starts at the beginning of the third row, and so on. Here is a drawing of how the
-            # LEDs are aligned: 
+            # LEDs are aligned:
             if input.r[i][j] == 0 and input.g[i][j] == 0 and input.b[i][j] == 0:
                 continue
 
@@ -43,10 +46,4 @@ def LED_MATRIX_WS2812B(dc_inputs: list[DataContainer], params: dict) -> dict:
 
     arduino.write((cmd[:-1] + "\n").encode())
     # arduino.close()
-    return input # return the input so that the next node can use it
-
-
-
-
-
-
+    return input  # return the input so that the next node can use it
