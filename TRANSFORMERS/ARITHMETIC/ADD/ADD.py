92129f04d4a5c549e5a2ab7db8bf8536535bfdb4
Jeff Zhang <47371088+JeffDotPng@users.noreply.github.com>
2023-07-05 00:57:25 -0400

    Migrate ADD node to new API with arbitrary inputs (#151)

--- TRANSFORMERS/ARITHMETIC/ADD/ADD.py
+++ TRANSFORMERS/ARITHMETIC/ADD/ADD.py
@@ -1,22 +1,19 @@
 import numpy as np
 from flojoy import flojoy, OrderedPair
+from functools import reduce
 
 
 @flojoy
-def ADD(a: OrderedPair, b: OrderedPair) -> OrderedPair:
+def ADD(a: OrderedPair, b: list[OrderedPair]) -> OrderedPair:
     """Add 2 or more numeric arrays, matrices, dataframes, or constants element-wise.
     When a constant is added to an array or matrix, each element in the array or
     matrix will be increased by the constant value. If 2 arrays or matrices of different
     sizes are added, the output will be the size of the larger array or matrix with
     only the overlapping elements changed.
     """
-    # TODO: Add support for adding more than 2 elements
     # TODO: Support other data types
     x = a.x
 
-    u = a.y
-    v = b.y
-
-    y = np.add(u, v)
+    y = reduce(lambda u, v: np.add(u, v.y), b, a.y)
 
     return OrderedPair(x=x, y=y)
