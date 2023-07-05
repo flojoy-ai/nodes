9258c6ac210cdd8a6685b83ec5959f2a924941e6
JeffDotPng <zhangjeff2014@gmail.com>
2023-07-05 09:34:56 -0400

    Remove default values from NodeReference

--- GENERATORS/SIMULATIONS/FEEDBACK/FEEDBACK.py
+++ GENERATORS/SIMULATIONS/FEEDBACK/FEEDBACK.py
@@ -2,9 +2,7 @@ from flojoy import flojoy, DataContainer, get_job_result, NodeReference
 
 
 @flojoy
-def FEEDBACK(
-    default: DataContainer, referred_node: NodeReference = NodeReference("")
-) -> DataContainer:
+def FEEDBACK(default: DataContainer, referred_node: NodeReference) -> DataContainer:
     result = get_job_result(referred_node.ref)
     if result:
         return result
--- LOGIC_GATES/LOOPS/GOTO/GOTO.py
+++ LOGIC_GATES/LOOPS/GOTO/GOTO.py
@@ -2,6 +2,6 @@ from flojoy import DataContainer, JobResultBuilder, flojoy, NodeReference
 
 
 @flojoy
-def GOTO(default: DataContainer, goto_node_id: NodeReference = NodeReference("")):
-    next_nodes = [goto_node_id.ref] if goto_node_id.ref != "" else []
+def GOTO(default: DataContainer, goto_node_id: NodeReference):
+    next_nodes = [goto_node_id.ref] if goto_node_id else []
     return JobResultBuilder().from_inputs([default]).flow_to_nodes(next_nodes).build()
