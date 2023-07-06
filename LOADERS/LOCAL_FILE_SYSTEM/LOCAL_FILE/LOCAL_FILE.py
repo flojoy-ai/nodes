268f660166d3dc8c926e0ca67728353f7b524df6
Syed Mahbub <86223939+smahmed776@users.noreply.github.com>
2023-07-06 02:36:09 +0100

    Mahbub node migration (#154)

--- LOADERS/LOCAL_FILE_SYSTEM/LOCAL_FILE/LOCAL_FILE.py
+++ LOADERS/LOCAL_FILE_SYSTEM/LOCAL_FILE/LOCAL_FILE.py
@@ -1,23 +1,24 @@
-from flojoy import flojoy, DataContainer, JobResultBuilder
-from typing import Union
+from flojoy import flojoy, Image, DataFrame
+from typing import Literal
 import numpy as np
-from PIL import Image
+from PIL import Image as PIL_Image
 from os import path
 import pandas as pd
 
 
-def get_file_path(file_path: str, default_path: str = None) -> str:
-    if default_path is None and file_path == "":
+def get_file_path(file_path: str, default_path: str | None = None):
+    f_path = path.abspath(file_path) if file_path != "" else default_path
+    if not f_path:
         raise ValueError("File path is missing for file_path parameter!")
-    f_path = file_path if file_path != "" else default_path
     print(f"file will be loaded from {f_path}")
     return f_path
 
 
-@flojoy
+@flojoy(deps={"xlrd": "2.0.1", "lxml": "4.9.2"})
 def LOCAL_FILE(
-    dc_inputs: list[DataContainer], params: dict
-) -> Union[DataContainer, dict]:
+    file_path: str,
+    file_type: Literal["Image", "JSON", "CSV", "Excel", "XML"] = "Image",
+) -> Image | DataFrame:
     """The LOCAL_FILE node loads a local file of different type and converts it to a DataContainer class.
 
     Parameters
@@ -35,17 +36,15 @@ def LOCAL_FILE(
         type 'dataframe' for file_type 'json', 'csv', 'excel', 'xml'
 
     """
-    file_type: str = params["file_type"]
-    file_path: str = params["path"]
     match file_type:
-        case "image":
+        case "Image":
             default_image_path = path.join(
                 path.dirname(path.abspath(__file__)),
                 "assets",
                 "astronaut.png",
             )
             file_path = get_file_path(file_path, default_image_path)
-            f = Image.open(file_path)
+            f = PIL_Image.open(file_path)
             img_array = np.array(f.convert("RGBA"))
             red_channel = img_array[:, :, 0]
             green_channel = img_array[:, :, 1]
@@ -54,30 +53,25 @@ def LOCAL_FILE(
                 alpha_channel = img_array[:, :, 3]
             else:
                 alpha_channel = None
-            return DataContainer(
-                type="image",
+            return Image(
                 r=red_channel,
                 g=green_channel,
                 b=blue_channel,
                 a=alpha_channel,
             )
-        case "csv":
+        case "CSV":
             file_path = get_file_path(file_path)
             df = pd.read_csv(file_path)
-            return DataContainer(type="dataframe", m=df)
-        case "json":
+            return DataFrame(df=df)
+        case "JSON":
             file_path = get_file_path(file_path)
             df = pd.read_json(file_path)
-            return DataContainer(type="dataframe", m=df)
-        case "xml":
+            return DataFrame(df=df)
+        case "XML":
             file_path = get_file_path(file_path)
             df = pd.read_xml(file_path)
-            return DataContainer(type="dataframe", m=df)
-        case "excel":
+            return DataFrame(df=df)
+        case "Excel":
             file_path = get_file_path(file_path)
             df = pd.read_excel(file_path)
-            return DataContainer(type="dataframe", m=df)
-        case _:
-            raise ValueError(
-                f"LOCAL_FILE currently doesn't support file type : {file_type}"
-            )
+            return DataFrame(df=df)
