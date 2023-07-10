5499bc3bef585f9501605116b32f14dd58763e1c
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-07-10 15:51:13 -0400

    Loaders nodes jin (#159)
    * Loader nodes syntax updated
    
    * style

--- LOADERS/CLOUD_DATABASE/LOADER/LOADER.py
+++ LOADERS/CLOUD_DATABASE/LOADER/LOADER.py
@@ -9,27 +9,26 @@ MEASUREMENT_API: str = f"{FRONTIER_URI}/api/streaming"
 
 
 @flojoy
-def LOADER(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
+def LOADER(default: DataContainer, measurement_uuid: str = "") -> DataContainer:
     api_key: str | None = get_frontier_api_key()
     if api_key is None:
         raise KeyError(f"Frontier API key is not found!")
 
-    measurement_uuid: str = params["measurement_uuid"]
     if measurement_uuid == "":
         raise KeyError(f"Measurement ID is not found!")
 
-    if dc_inputs:
+    if default:
         resp = requests.post(
             MEASUREMENT_API,
             json={
                 "api_key": api_key,
                 "measurement_id": measurement_uuid,
-                "measurement": json.dumps(dc_inputs[0], cls=PlotlyJSONEncoder),
+                "measurement": json.dumps(default, cls=PlotlyJSONEncoder),
             },
         )
         if not (resp.status_code == 200 or resp.status_code == 201):
             raise Exception(str(resp.json()["error"]))
-        return dc_inputs[0]
+        return default
 
     else:
         resp = requests.get(
--- LOADERS/REDIS_SYSTEM/REDIS_LOAD/REDIS_LOAD.py
+++ LOADERS/REDIS_SYSTEM/REDIS_LOAD/REDIS_LOAD.py
@@ -1,4 +1,4 @@
-from flojoy import flojoy, JobResultBuilder, DataContainer
+from flojoy import flojoy, JobResultBuilder, DataContainer, OrderedPair, NodeReference
 import os
 from redis import Redis
 from rq.job import Job, NoSuchJobError
@@ -11,7 +11,9 @@ REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 
 
 @flojoy
-def REDIS_LOAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
+def REDIS_LOAD(
+    default: OrderedPair, referred_node: NodeReference = ""
+) -> DataContainer:
     """The REDIS_LOAD node loads data directly from REDIS.
 
     Parameters
@@ -24,8 +26,7 @@ def REDIS_LOAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
     dataframe
         The dataframe loaded from Redis. Ordered pair.
     """
-    referred_node = params["referred_node"]
-    x = dc_inputs[0].y
+    x = default.y
     if referred_node != "":
         referred_node_key = referred_node.split("-")[0]
         try:
@@ -41,7 +42,7 @@ def REDIS_LOAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
             )
 
         except (Exception, NoSuchJobError):
-            y = x if len(dc_inputs) > 0 else [1, 3, 2]
+            y = x
             print(traceback.format_exc())
             pass
         return (
@@ -50,4 +51,4 @@ def REDIS_LOAD(dc_inputs: list[DataContainer], params: dict) -> DataContainer:
             .build()
         )
     else:
-        return JobResultBuilder().from_inputs(dc_inputs).build()
+        return JobResultBuilder().from_inputs(default).build()
