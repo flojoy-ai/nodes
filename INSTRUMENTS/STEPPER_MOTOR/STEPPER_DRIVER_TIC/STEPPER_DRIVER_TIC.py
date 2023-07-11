e7acb38b5fe28e9046a7c1a5e53b3218ccf1824d
angelchauffray <ac@enclose.solar>
2023-07-11 17:51:13 -0400

    reformat files

--- INSTRUMENTS/STEPPER_MOTOR/STEPPER_DRIVER_TIC/STEPPER_DRIVER_TIC.py
+++ INSTRUMENTS/STEPPER_MOTOR/STEPPER_DRIVER_TIC/STEPPER_DRIVER_TIC.py
@@ -7,17 +7,15 @@ from time import sleep
 
 @flojoy(deps={"ticlib": "0.2.2"})
 def STEPPER_DRIVER_TIC(
-        default: OrderedPair = None,
-        current_limit: int = 30,
-        sleep_time: int = 2) -> OrderedPair:
+    default: OrderedPair = None, current_limit: int = 30, sleep_time: int = 2
+) -> OrderedPair:
     """
     Takes current limit and sleep time as parameters and allow to control position
     and speed of a motor with a TIC driver
     """
 
     positions: list[int] = [50, 100, 150, 200]  # Setting default positions
-    speeds: list[int] = [50000, 1000000, 150000,
-                         200000]  # Setting default speeds
+    speeds: list[int] = [50000, 1000000, 150000, 200000]  # Setting default speeds
     current_limit: int = current_limit
     sleep_time: int = sleep_time
 
