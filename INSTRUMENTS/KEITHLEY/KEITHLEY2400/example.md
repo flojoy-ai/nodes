7d72689c8a0c70f29d56bec3892994cfbd470d29
angelchauffray <ac@enclose.solar>
2023-07-12 13:16:31 -0400

    resolved changes

--- INSTRUMENTS/KEITHLEY/KEITHLEY2400/example.md
+++ INSTRUMENTS/KEITHLEY/KEITHLEY2400/example.md
@@ -15,6 +15,6 @@ You need four nodes to record an IV curve :
 
 - The ['LINE'](https://github.com/flojoy-io/nodes/blob/main/VISUALIZERS/PLOTLY/LINE/LINE.py) node will display the I-V curve by plotting the currents received from the device in function of the voltages transmitted to the device.
 
-- The ['END'](https://github.com/flojoy-io/nodes/blob/main/LOGIC_GATES/TERMINATORS/END.py) node terminates the process.
+- The ['END'](https://github.com/flojoy-io/nodes/blob/main/LOGIC_GATES/TERMINATORS/END/END.py) node terminates the process.
 
 When the setup is ready, and the parameters above are well defined, you can start the experiment (Turn on the source meter and click the 'PLAY' button).
--- INSTRUMENTS/STEPPER_MOTOR/STEPPER_DRIVER_TIC/STEPPER_DRIVER_TIC.py
+++ INSTRUMENTS/STEPPER_MOTOR/STEPPER_DRIVER_TIC/STEPPER_DRIVER_TIC.py
@@ -1,4 +1,5 @@
 from flojoy import flojoy, OrderedPair
+from typing import Optional
 from ticlib import (
     TicUSB,
 )  # Import the TicUSB library to send command to Tic drivers with USB connection
@@ -7,17 +8,28 @@ from time import sleep
 
 @flojoy(deps={"ticlib": "0.2.2"})
 def STEPPER_DRIVER_TIC(
-    default: OrderedPair = None, current_limit: int = 30, sleep_time: int = 2
+    default: Optional[OrderedPair] = None, current_limit: int = 30, sleep_time: int = 2, speed: int = 100000,
 ) -> OrderedPair:
-    """
-    Takes current limit and sleep time as parameters and allow to control position
-    and speed of a motor with a TIC driver
+   """
+   This node controls a stepper motor movement with a TIC driver.
+  
+   The user defines the speed and the sleep time between movements. 
+   (To choose the position, use the STEPPER_DRIVER_TIC_KNOB)
+
+
+   Parameters :
+   ------------
+   current_limit: int
+       Defines the current limitation that the stepper motor will receive. 
+
+   sleep_time: int
+       Defines the sleep time after moving to each position.
+
+   speed: int
+       Defines the speed of the motor movement. (Between 0 and 200000)
     """
 
     positions: list[int] = [50, 100, 150, 200]  # Setting default positions
-    speeds: list[int] = [50000, 1000000, 150000, 200000]  # Setting default speeds
-    current_limit: int = current_limit
-    sleep_time: int = sleep_time
 
     # Declaration of the stepper driver
     tic: TicUSB = TicUSB()
@@ -28,13 +40,13 @@ def STEPPER_DRIVER_TIC(
     tic.exit_safe_start()  # The driver is now ready to receive commands
 
     for i in range(0, len(positions)):
-        tic.set_max_speed(speeds[i])  # Set motor speed
+        tic.set_max_speed(speeds)  # Set motor speed
         tic.set_target_position(positions[i])  # Set target positions
         sleep(sleep_time)
 
     tic.deenergize()
     tic.enter_safe_start()
 
-    position_parameters = OrderedPair(x=speeds, y=positions)
+    position_parameters = OrderedPair(x=positions, y=positions)
 
     return position_parameters
