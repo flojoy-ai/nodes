86d7fd804b2beff4d0c68e2e91f54fd66e41aede
Jinwon Lee <jinwonlee0916@gmail.com>
2023-07-13 19:16:59 -0400

    Started DF_2_OT and updated DF_2_NP

--- TRANSFORMERS/TYPE_CASTING/DF_2_OrderedTriple/DF_2_OrderedTriple.py
+++ TRANSFORMERS/TYPE_CASTING/DF_2_OrderedTriple/DF_2_OrderedTriple.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from flojoy import flojoy, DataFrame, Matrix
+from flojoy import flojoy, DataFrame, OrderedTriple
 
 
 @flojoy
-def DF_2_NP(default: DataFrame) -> Matrix:
+def DF_2_OrderedTriple(default: DataFrame) -> OrderedTriple:
     """
     Node to convert dataframe type data into matrix type data.
     It takes one dataframe type data and converts it to matrix type data.
@@ -21,4 +21,4 @@ def DF_2_NP(default: DataFrame) -> Matrix:
     df = default.m
     df_to_numpy = df.to_numpy(dtype=object)
 
-    return Matrix(m=df_to_numpy)
+    return OrderedTriple(m=df_to_numpy)
