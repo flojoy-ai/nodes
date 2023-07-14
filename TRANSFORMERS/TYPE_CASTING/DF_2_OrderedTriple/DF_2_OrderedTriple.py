1a85560158c628828d24c2cc90e8e0be7c532cc1
Jinwon Lee <jinwonlee0916@gmail.com>
2023-07-14 11:25:00 -0400

    OrderedTriple created

--- TRANSFORMERS/TYPE_CASTING/DF_2_OrderedTriple/DF_2_OrderedTriple.py
+++ TRANSFORMERS/TYPE_CASTING/DF_2_OrderedTriple/DF_2_OrderedTriple.py
@@ -1,24 +1,35 @@
-import pandas as pd
 from flojoy import flojoy, DataFrame, OrderedTriple
 
 
 @flojoy
-def DF_2_OrderedTriple(default: DataFrame) -> OrderedTriple:
+def DF_2_OrderedTriple(default: DataFrame, x: int = 0, y: int = 1, z: int = 2) -> OrderedTriple:
     """
-    Node to convert dataframe type data into matrix type data.
-    It takes one dataframe type data and converts it to matrix type data.
+    Node to convert dataframe type data into OrderedTriple type data.
+    It takes one dataframe type data to picks 3 different columns to generate OrderedTriple type.
 
     Parameters
     ----------
-    None
+    x: the index of column that represents x axis
+    y: the index of column that represents y axis
+    z: the index of column that represents z axis
 
     Returns
     -------
-    numpy array
-        Converted dataframe value from the input
+    OrderTriple, x, y ,z
     """
 
     df = default.m
-    df_to_numpy = df.to_numpy(dtype=object)
+    if df.shape[1] < 3:
+        raise AssertionError("Unable to convert it to OrderedTriple type")
+    
+    x_list = df.iloc[:, x]
+    y_list = df.iloc[:, y]
+    z_list = df.iloc[:, z]
 
-    return OrderedTriple(m=df_to_numpy)
+    x_numpy = x_list.to_numpy(dtype=object)
+    y_numpy = y_list.to_numpy(dtype=object)
+    z_numpy = z_list.to_numpy(dtype=object)
+
+
+    # return OrderedTriple(x=[1], y=[1], z=[1])
+    return OrderedTriple(x=x_numpy, y=y_numpy, z=z_numpy)
