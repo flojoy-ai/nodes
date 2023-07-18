fe898909bda5e51aedef863bc1c28d2392b72b24
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-07-18 08:03:54 -0400

    fix DEEPLAB_V3 tests

--- AI_ML/SEGMENTATION/DEEPLAB_V3/DEEPLAB_V3_test_.py
+++ AI_ML/SEGMENTATION/DEEPLAB_V3/DEEPLAB_V3_test_.py
@@ -2,17 +2,10 @@ import os
 import pytest
 from unittest.mock import patch
 
-import requests
-from PIL import Image
+from PIL import Image as PIL_Image
 import numpy as np
 
-from flojoy import DataContainer
-
-
-@pytest.fixture(scope="module")
-def mock_flojoy_decorator():
-    with patch("flojoy.flojoy", lambda x: x) as mock_flojoy:
-        yield mock_flojoy
+from flojoy import Image
 
 
 @pytest.fixture
@@ -20,16 +13,14 @@ def obama_image_array_rgb():
     _image_path = (
         f"{os.path.dirname(os.path.realpath(__file__))}/assets/obama_image.jpg"
     )
-    image = Image.open(_image_path).convert("RGB")
+    image = PIL_Image.open(_image_path).convert("RGB")
     return np.array(image)
 
 
 @pytest.fixture
 def obama_segmentation_array_rgb():
-    _image_path = (
-        f"{os.path.dirname(os.path.realpath(__file__))}/obama_segmentation_image.jpg"
-    )
-    image = Image.open(_image_path).convert("RGB")
+    _image_path = f"{os.path.dirname(os.path.realpath(__file__))}/assets/obama_segmentation_image.jpg"
+    image = PIL_Image.open(_image_path).convert("RGB")
     return np.array(image)
 
 
@@ -38,14 +29,13 @@ def test_DEEPLAB_V3(
 ):
     import DEEPLAB_V3
 
-    input_dc = DataContainer(
-        type="image",
+    input_image = Image(
         r=obama_image_array_rgb[:, :, 0],
         g=obama_image_array_rgb[:, :, 1],
         b=obama_image_array_rgb[:, :, 2],
         a=None,
     )
-    output_dc = DEEPLAB_V3.DEEPLAB_V3(dc_inputs=[input_dc], params={})
+    output_dc = DEEPLAB_V3.DEEPLAB_V3(default=input_image)  # type: ignore
     output_segmentation = np.stack((output_dc.r, output_dc.g, output_dc.b), axis=2)
 
     # Threshold both segmentation masks
