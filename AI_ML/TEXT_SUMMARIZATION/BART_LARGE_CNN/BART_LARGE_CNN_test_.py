3b198e71092a05016faad967e3f2556bd20f399c
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-07-21 19:27:49 -0400

    fix

--- AI_ML/TEXT_SUMMARIZATION/BART_LARGE_CNN/BART_LARGE_CNN_test_.py
+++ AI_ML/TEXT_SUMMARIZATION/BART_LARGE_CNN/BART_LARGE_CNN_test_.py
@@ -1,4 +1,6 @@
 import os
+import tempfile
+from unittest.mock import patch
 import pytest
 
 import pandas as pd
@@ -16,11 +18,13 @@ def long_text():
 
 @pytest.mark.slow
 def test_BART_LARGE_CNN(mock_flojoy_decorator, long_text):
-    import BART_LARGE_CNN
+    with tempfile.TemporaryDirectory() as tempdir:
+        with patch("flojoy.flojoy_node_venv._get_venv_cache_dir", return_value=tempdir) as mock_venv_cache_dir:
+            import BART_LARGE_CNN
 
-    output = BART_LARGE_CNN.BART_LARGE_CNN(
-        DataFrame(df=pd.DataFrame({"text": [long_text]}))
-    )
-    assert isinstance(output, DataFrame)
-    assert output.m.shape == (3, 1)
-    assert output.m.columns == ["summary_text"]
+            output = BART_LARGE_CNN.BART_LARGE_CNN(
+                DataFrame(df=pd.DataFrame({"text": [long_text]}))
+            )
+            assert isinstance(output, DataFrame)
+            assert output.m.shape == (1, 1)
+            assert output.m.columns == ["summary_text"]
