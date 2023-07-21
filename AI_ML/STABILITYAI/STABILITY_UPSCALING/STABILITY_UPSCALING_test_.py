ab4809e677d10a5a24ded903fe3b282ad394ad02
Vinicius <vinicvaz95@gmail.com>
2023-07-21 08:26:48 -0300

    update upscaling test

--- AI_ML/STABILITYAI/STABILITY_UPSCALING/STABILITY_UPSCALING_test_.py
+++ AI_ML/STABILITYAI/STABILITY_UPSCALING/STABILITY_UPSCALING_test_.py
@@ -3,33 +3,19 @@ import numpy
 from functools import wraps
 from unittest.mock import patch
 
+import pytest
 import numpy as np
-import pandas as pd
 from flojoy import DataContainer
 from PIL import Image
 from pathlib import Path
 
-
-def mock_flojoy_decorator(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-
-# Patch the flojoy decorator that handles connecting our node to the App.
-patch("flojoy.flojoy", mock_flojoy_decorator).start()
-
-# After Patching the flojoy decorator, let's load the node under test.
-import STABILITY_UPSCALING
-
-
-def test_STABILITY_UPSCALING():
+def test_STABILITY_UPSCALING(mock_flojoy_decorator):
+    import STABILITY_UPSCALING
     image_path = Path(__file__).parent / "test_img.png"
     output_width = 1024
     res = STABILITY_UPSCALING.STABILITY_UPSCALING(
-        [DataContainer()], {"output_width": output_width, "image_path": image_path}
+        output_width=output_width,
+        image_path=image_path
     )
 
     assert res.type == 'image'
