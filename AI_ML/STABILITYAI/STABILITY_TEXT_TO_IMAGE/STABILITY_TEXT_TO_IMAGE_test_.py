52089f81987b5929e321426b62015f760ea6c078
Vinicius <vinicvaz95@gmail.com>
2023-07-21 08:30:29 -0300

    update text2img tests

--- AI_ML/STABILITYAI/STABILITY_TEXT_TO_IMAGE/STABILITY_TEXT_TO_IMAGE_test_.py
+++ AI_ML/STABILITYAI/STABILITY_TEXT_TO_IMAGE/STABILITY_TEXT_TO_IMAGE_test_.py
@@ -1,34 +1,17 @@
 import numpy
-
-from functools import wraps
-from unittest.mock import patch
-
-import numpy as np
-import pandas as pd
-from flojoy import DataContainer
+import pytest
 
 
-def mock_flojoy_decorator(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        return f(*args, **kwargs)
 
-    return decorated_function
-
-
-# Patch the flojoy decorator that handles connecting our node to the App.
-patch("flojoy.flojoy", mock_flojoy_decorator).start()
-
-# After Patching the flojoy decorator, let's load the node under test.
-import STABILITY_TEXT_TO_IMAGE
-
-
-def test_STABILITY_TEXT_TO_IMAGE():
+def test_STABILITY_TEXT_TO_IMAGE(mock_flojoy_decorator):
+    import STABILITY_TEXT_TO_IMAGE
     prompt = "A painting of a cat"
     width = 512
     height = 512
     res = STABILITY_TEXT_TO_IMAGE.STABILITY_TEXT_TO_IMAGE(
-        [DataContainer()], {"prompt": prompt, "width": width, "height": height}
+       prompt=prompt, 
+       width=width,
+       height=height
     )
 
     assert res.type == 'image'
