7c18dc3b0d0af02f819b68c674e70dd41078e943
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-07-21 08:56:32 -0400

    fix IFFT tests

--- TRANSFORMERS/SIGNAL_PROCESSING/IFFT/IFFT_test_.py
+++ TRANSFORMERS/SIGNAL_PROCESSING/IFFT/IFFT_test_.py
@@ -1,33 +1,13 @@
 import numpy as np
 from scipy import fft
-from pandas import DataFrame
+import pandas as pd
 
-from functools import wraps
-from unittest.mock import patch
+from flojoy import DataFrame
 
-from flojoy import DataContainer
+def test_IFFT(mock_flojoy_decorator):
 
+    import IFFT
 
-# Python functions are decorated at module-loading time, So we'll need to patch our decorator
-#  with a simple mock ,before loading the module.
-
-
-def mock_flojoy_decorator(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-
-# Patch the flojoy decorator that handles connecting our node to the App.
-patch("flojoy.flojoy", mock_flojoy_decorator).start()
-
-# After Patching the flojoy decorator, let's load the node under test.
-import IFFT
-
-
-def test_IFFT():
     N = 600
     T = 1.0 / 800.0
     x = np.linspace(0.0, N * T, N, endpoint=False)
@@ -35,8 +15,8 @@ def test_IFFT():
     fourier = fft.fft(y)
 
     d = {"x": x, "real": fourier.real, "imag": fourier.imag}
-    element = DataContainer(type="dataframe", m=DataFrame(data=d))
-    res = IFFT.IFFT([element], {"real_signal": False})
+    element = DataFrame(df=pd.DataFrame(data=d))
+    res = IFFT.IFFT(default=element, real_signal=False)
 
     original = fft.ifft(fourier).real
     assert (x == res.x).all()
