386f3fe57f54c9fe248348e4e5cc7c609aed728b
Joey Yu <joey@itsjoeoui.com>
2023-07-26 13:59:46 -0400

    Fix constant logic

--- GENERATORS/SIMULATIONS/CONSTANT/CONSTANT.py
+++ GENERATORS/SIMULATIONS/CONSTANT/CONSTANT.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flojoy import flojoy, Vector, OrderedPair
+from flojoy import DCNpArrayType, flojoy, Vector, OrderedPair
 from typing import Optional
 
 
@@ -8,9 +8,13 @@ def CONSTANT(
     default: Optional[Vector] = None, constant: float = 3.0, step: int = 1000
 ) -> OrderedPair:
     """Generates a single x-y vector of numeric (floating point) constants"""
+
+    x: DCNpArrayType
     if default:
         x = default.v
-    x = np.arange(0, step, 1)  # type: ignore
+    else:
+        x = np.arange(0, step, 1)
 
     y = np.full(len(x), constant)
+
     return OrderedPair(x=x, y=y)
