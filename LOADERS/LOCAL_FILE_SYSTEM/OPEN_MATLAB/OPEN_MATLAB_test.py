4e79c23f0e34b13639bc716b9d6b6113bf76be4f
Jinwon Lee <jinwonlee0916@gmail.com>
2023-07-26 18:39:00 -0400

    Update tests

--- LOADERS/LOCAL_FILE_SYSTEM/OPEN_MATLAB/OPEN_MATLAB_test.py
+++ LOADERS/LOCAL_FILE_SYSTEM/OPEN_MATLAB/OPEN_MATLAB_test.py
@@ -2,16 +2,11 @@ import os
 import pytest
 
 
-@pytest.fixture
-def output_shape():
-    return (1831, 22)
-
-
 @pytest.mark.slow
-def test_OPEN_MATLAB(mock_flojoy_decorator, mock_flojoy_cache_directory, output_shape):
+def test_OPEN_MATLAB(mock_flojoy_decorator, mock_flojoy_cache_directory):
     import OPEN_MATLAB
 
     _file_path = f"{os.path.dirname(os.path.realpath(__file__))}/assets/default.mat"
     output = OPEN_MATLAB.OPEN_MATLAB(_file_path)
 
-    assert output.m.shape == output_shape
+    assert output.m.shape == (1831, 22)
