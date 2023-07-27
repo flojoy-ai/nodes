3a5d9e3dd56bd5124d061716033eca7499b41cf3
dstrande <dstrande@live.ca>
2023-07-27 10:12:00 -0700

    black caused test to fail..

--- GENERATORS/SIMULATIONS/SECOND_ORDER_SYSTEM/SECOND_ORDER_SYSTEM_test_.py
+++ GENERATORS/SIMULATIONS/SECOND_ORDER_SYSTEM/SECOND_ORDER_SYSTEM_test_.py
@@ -12,7 +12,7 @@ def test_SECOND_ORDER_SYSTEM(mock_flojoy_decorator):
 
     # TODO: rewrite this test using a mock SmallMemory.
     x = np.linspace(0, 10, 1000)
-    v = (Vector(v=x),)
+    v = Vector(v=x)
 
     res = SECOND_ORDER_SYSTEM.SECOND_ORDER_SYSTEM(default=v, default_params=defaultP)
 
