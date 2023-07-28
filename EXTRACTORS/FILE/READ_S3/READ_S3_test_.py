a56b8a557341e57830fcdbadfd5485c52b218a7c
Youngsun Yoo <youngsun123q@gmail.com>
2023-07-28 16:48:05 -0400

    ran black

--- EXTRACTORS/FILE/READ_S3/READ_S3_test_.py
+++ EXTRACTORS/FILE/READ_S3/READ_S3_test_.py
@@ -5,32 +5,24 @@ import moto
 import pandas as pd
 from flojoy import DataFrame
 
+
 # Test dataframe to write as parquet file
 @pytest.fixture
 def test_dataframe():
     return pd.DataFrame.from_dict({"a": [1, 2, 3], "b": [4, 5, 6]})
 
 
-
-def test_READ_S3(
-    mock_flojoy_decorator,
-    mock_flojoy_cache_directory,
-    test_dataframe
-    ):
-
-    
+def test_READ_S3(mock_flojoy_decorator, mock_flojoy_cache_directory, test_dataframe):
     with moto.mock_s3():
-
         import boto3
 
-        conn = boto3.resource('s3')
+        conn = boto3.resource("s3")
         conn.create_bucket(Bucket="test_bucket")
         test_bucket = conn.Bucket("test_bucket")
         # Write a parquet file to the bucket
         with io.BytesIO() as f:
             test_dataframe.to_parquet(f)
             test_bucket.put_object(Key="test_df.parquet", Body=f.getvalue())
-
 
         import READ_S3
 
