b9de15162d0eea51e49ef03bce94dac205ae3b3d
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-07-28 16:40:15 -0400

    fix boto3 import

--- EXTRACTORS/FILE/READ_S3/READ_S3_test_.py
+++ EXTRACTORS/FILE/READ_S3/READ_S3_test_.py
@@ -1,5 +1,6 @@
 import pytest
 import io
+import boto3
 import moto
 import pandas as pd
 from flojoy import DataFrame
@@ -10,13 +11,18 @@ def test_dataframe():
     return pd.DataFrame.from_dict({"a": [1, 2, 3], "b": [4, 5, 6]})
 
 
-# This creates a fake bucket under s3://test_bucket with a file under s3://test_bucket/test_df.parquet
-@pytest.fixture
-def mock_bucket(test_dataframe):
-    moto_fake = moto.mock_s3()
-    try:
-        moto_fake.start()
+
+def test_READ_S3(
+    mock_flojoy_decorator,
+    mock_flojoy_cache_directory,
+    test_dataframe
+    ):
+
+    
+    with moto.mock_s3():
+
         import boto3
+
         conn = boto3.resource('s3')
         conn.create_bucket(Bucket="test_bucket")
         test_bucket = conn.Bucket("test_bucket")
@@ -24,19 +30,9 @@ def mock_bucket(test_dataframe):
         with io.BytesIO() as f:
             test_dataframe.to_parquet(f)
             test_bucket.put_object(Key="test_df.parquet", Body=f.getvalue())
-        yield conn
-    finally:
-        moto_fake.stop()
 
 
-def test_READ_S3(
-    mock_flojoy_decorator,
-    mock_flojoy_cache_directory,
-    mock_bucket,
-    test_dataframe
-    ):
+        import READ_S3
 
-    import READ_S3
-
-    output = READ_S3.READ_S3("test", "test_bucket", "test_df.parquet")
-    assert output.m.equals(test_dataframe)
+        output = READ_S3.READ_S3("test", "test_bucket", "test_df.parquet")
+        assert output.m.equals(test_dataframe)
