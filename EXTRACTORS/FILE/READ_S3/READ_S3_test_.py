1b362609574654cfb542a31e97614e9b6481d05d
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-07-28 17:26:56 -0400

    run black

--- EXTRACTORS/FILE/READ_S3/READ_S3_test_.py
+++ EXTRACTORS/FILE/READ_S3/READ_S3_test_.py
@@ -6,6 +6,7 @@ import moto
 import pandas as pd
 from flojoy import DataFrame
 
+
 # Test dataframe to write as parquet file
 @pytest.fixture
 def test_dataframe():
@@ -16,7 +17,7 @@ def test_dataframe():
 @pytest.fixture
 def mock_bucket(test_dataframe):
     with moto.mock_s3() as moto_fake:
-        conn = boto3.resource('s3')
+        conn = boto3.resource("s3")
         conn.create_bucket(Bucket="test_bucket")
         test_bucket = conn.Bucket("test_bucket")
         # Write a parquet file to the bucket
@@ -25,13 +26,13 @@ def mock_bucket(test_dataframe):
             test_bucket.put_object(Key="test_df.parquet", Body=f.getvalue())
         yield conn
 
+
 # Mock keyring to return None
 @pytest.fixture
 def mock_keyring_get_password():
-    # Mock keyring get_password et 
+    # Mock keyring get_password et
     with patch("keyring.get_password", return_value=None) as mock_keyright:
         yield mock_keyright
-
 
 
 def test_READ_S3(
@@ -40,8 +41,7 @@ def test_READ_S3(
     mock_bucket,
     mock_keyring_get_password,
     test_dataframe,
-    ):
-
+):
     import READ_S3
 
     output = READ_S3.READ_S3("test", "test_bucket", "test_df.parquet")
