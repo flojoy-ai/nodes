91362048ac95a032b4e260256fee80dd3cf4c03d
Jinwon Lee <jinwonlee0916@gmail.com>
2023-08-01 10:56:11 -0400

    Update DOUBLE_DEFINITE_INTEGRAL

--- TRANSFORMERS/CALCULUS/DOUBLE_DEFINITE_INTEGRAL/DOUBLE_DEFINITE_INTEGRAL_test_.py
+++ TRANSFORMERS/CALCULUS/DOUBLE_DEFINITE_INTEGRAL/DOUBLE_DEFINITE_INTEGRAL_test_.py
@@ -1,9 +1,7 @@
 def test_DOUBLE_DEFINITE_INTEGRAL(mock_flojoy_decorator):
     import DOUBLE_DEFINITE_INTEGRAL
-    import numpy as np
 
     function = "2*x*y"
     node = DOUBLE_DEFINITE_INTEGRAL.DOUBLE_DEFINITE_INTEGRAL(function, 1, 0, 1, 0)
 
-    print(node.c)
-    assert np.array_equal(np.array([0.5]), node.c)
+    assert 0.5 == round(node.c[0], 1)
