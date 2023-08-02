2b49582c78ff6586a1cb5d6da048a575e28ae3e9
Hristo <sandevh@gmail.com>
2023-08-02 17:51:57 -0400

    previous changes applied to arithmetic_utils.py

--- TRANSFORMERS/ARITHMETIC/utils/arithmetic_utils.py
+++ TRANSFORMERS/ARITHMETIC/utils/arithmetic_utils.py
@@ -4,10 +4,9 @@ from flojoy import OrderedPair, Scalar, Vector
 def get_val(
     data_container: OrderedPair | Scalar | Vector,
 ) -> OrderedPair | Scalar | Vector:
-    match data_container:
-        case OrderedPair():
-            return data_container.y
-        case Scalar():
-            return data_container.c
-        case Vector():
-            return data_container.v
+    if isinstance(data_container, OrderedPair):
+        return data_container.y
+    elif isinstance(data_container, Scalar):
+        return data_container.c
+    elif isinstance(data_container, Vector):
+        return data_container.v
