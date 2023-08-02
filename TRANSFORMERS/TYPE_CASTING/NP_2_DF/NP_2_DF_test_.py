0385eb9b3ec5305fd87e77b601b97eeaa9dc28c0
emiliedu <emilie.ducharme7@gmail.com>
2023-08-02 11:27:45 -0400

    Created type casting pytest files

---
+++ TRANSFORMERS/TYPE_CASTING/NP_2_DF/NP_2_DF_test_.py
@@ -0,0 +1,28 @@
+from flojoy import (
+        DataContainer,
+        DataFrame,
+        OrderedPair,
+        OrderedTriple,
+        Matrix,
+        Grayscale,
+        Image,
+        ParametricDataFrame,
+        ParametricOrderedPair,
+        ParametricOrderedTriple,
+        ParametricImage,
+        ParametricGrayscale,
+        ParametricMatrix,
+    )
+
+
+def test_NP_2_DF(mock_flojoy_decorator):
+    import NP_2_DF
+
+    #Test case DataFrame | ParametricDataFrame.
+    #Test case OrderedPair | ParametricOrderedPair.
+    #Test case OrderedTriple | ParametricOrderedTriple.
+    #Test case Matrix | ParametricMatrix.
+    #Test case Grayscale | ParametricGrayscale.
+    #Test case Image | ParametricImage.
+    #Test case when none and raise an error message.
+    #Check what is the Parametric variant and try to do one or two maybe.
---
+++ TRANSFORMERS/TYPE_CASTING/ORDERED_TRIPLE_2_SURFACE/ORDERED_TRIPLE_2_SURFACE_test_.py
@@ -0,0 +1,32 @@
+from flojoy import OrderedTriple, Surface
+import numpy as np
+
+
+def test_ORDERED_TRIPLE_2_SURFACE_general(mock_flojoy_decorator):
+    import ORDERED_TRIPLE_2_SURFACE
+
+    ordTriple = OrderedTriple(x=np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]), y=np.array([0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2]), z=np.array([0.0, 3.9, 2.4, 5.0, 1.2, 3.3, 3.4, 3.5, 0.9, 8.7, 5.6, 7.1]))
+    out = ORDERED_TRIPLE_2_SURFACE.ORDERED_TRIPLE_2_SURFACE(ordTriple)
+
+    np.testing.assert_array_equal(([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]), out.x)
+    np.testing.assert_array_equal(([[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2]]), out.y)
+    np.testing.assert_array_equal(([[0.0, 3.9, 2.4, 5.0], [1.2, 3.3, 3.4, 3.5], [0.9, 8.7, 5.6, 7.1]]), out.z)
+
+
+def test_ORDERED_TRIPLE_2_SURFACE_with_padding(mock_flojoy_decorator):
+    import ORDERED_TRIPLE_2_SURFACE
+
+    ordTriple = OrderedTriple(x=np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]), y=np.array([0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2]), z=np.array([0.0, 3.9, 2.4, 5.0, 1.2, 3.3, 3.4, 3.5, 0.9]))
+    out = ORDERED_TRIPLE_2_SURFACE.ORDERED_TRIPLE_2_SURFACE(ordTriple)
+
+    print(out.z)
+    assert len(out.z) == len(np.unique(ordTriple.y))
+    #Test with x,y unique and len(z) smaller??
+    #Test with x,y not unique and len(z) smaller??
+
+
+#def test_ORDERED_TRIPLE_2_SURFACE_no_padding(mock_flojoy_decorator):
+    #import ORDERED_TRIPLE_2_SURFACE
+
+    #Test with x,y unique and len(z) bigger??
+    #Test with x,y not unique and len(z) bigger
