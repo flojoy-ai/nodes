02d3d0d4dd5cc6cf644b50c0d51a7d1c17d7f9bc
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-11 18:05:39 -0400

    wip

--- GENERATORS/SIMULATIONS/FEEDBACK/FEEDBACK.py
+++ GENERATORS/SIMULATIONS/FEEDBACK/FEEDBACK.py
@@ -1,9 +1,14 @@
+from typing import Optional
 from flojoy import flojoy, DataContainer, get_job_result, NodeReference
 
 
 @flojoy
-def FEEDBACK(default: DataContainer, referred_node: NodeReference) -> DataContainer:
-    """The FEEDBACK node captures the result of the specified node ID. If the result is not found, it passes the result of the parent node.
+def FEEDBACK(
+    referred_node: NodeReference,
+    default: Optional[DataContainer] = None,
+) -> DataContainer:
+    """
+    The FEEDBACK node captures the result of the specified node ID. If the result is not found, it passes the result of the parent node.
 
     Parameters
     ----------
@@ -13,6 +18,7 @@ def FEEDBACK(default: DataContainer, referred_node: NodeReference) -> DataContai
 
     result = get_job_result(referred_node.ref)
     if result:
+        print(result)
         return result
     else:
         return default
