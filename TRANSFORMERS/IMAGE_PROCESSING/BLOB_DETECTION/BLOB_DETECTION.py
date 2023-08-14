e4b5ae66ab4df57e3cc870fbe9d0162784f16d35
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-14 13:03:57 -0400

    instantiated blob detection node

---
+++ TRANSFORMERS/IMAGE_PROCESSING/BLOB_DETECTION/BLOB_DETECTION.py
@@ -0,0 +1,51 @@
+from flojoy import flojoy, Image
+import cv2
+import numpy as np
+from PIL import ImageFilter, Image as PILImage
+
+
+@flojoy
+def BLOB_DETECTION(default: Image) -> Image:
+    """
+    The BLOB_DETECTION node detects edges in the image that was passed to it.
+
+    This is done through the Pillow image filter, FIND_EDGES.
+
+    Note that the image is converted to greyscale during this processing.
+
+    Returns
+    -------
+    image
+        The image with detected edges in white.
+    """
+
+    r = default.r
+    g = default.g
+    b = default.b
+    a = default.a
+
+    if a is not None:
+        rgba_image = np.stack((r, g, b, a), axis=2)
+
+    else:
+        rgba_image = np.stack((r, g, b), axis=2)
+
+    try:
+        image = PILImage.fromarray(rgba_image)
+        image = image.convert("L").filter(ImageFilter.FIND_EDGES).convert("RGB")
+        image = np.array(image)
+
+        try:
+            r, g, b, a = cv2.split(image)
+        except:
+            r, g, b = cv2.split(image)
+        if a is None:
+            a = None
+        return Image(
+            r=r,
+            g=g,
+            b=b,
+            a=a,
+        )
+    except Exception as e:
+        raise e
