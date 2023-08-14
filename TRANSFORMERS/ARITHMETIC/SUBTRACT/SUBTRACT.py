84e3b7a08cd56974c9ddf478d041fdc0adb6aad7
Joey Yu <joey@itsjoeoui.com>
2023-08-14 14:25:33 -0400

    format
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- TRANSFORMERS/ARITHMETIC/SUBTRACT/SUBTRACT.py
+++ TRANSFORMERS/ARITHMETIC/SUBTRACT/SUBTRACT.py
@@ -8,8 +8,7 @@ from functools import reduce
 def SUBTRACT(
     a: OrderedPair | Scalar | Vector, b: list[OrderedPair | Scalar | Vector]
 ) -> OrderedPair | Scalar | Vector:
-    """The SUBTRACT node subtracts two input vectors and returns the result.
-    """
+    """The SUBTRACT node subtracts two input vectors and returns the result."""
 
     initial = get_val(a)
     seq = map(lambda dc: get_val(dc), b)
