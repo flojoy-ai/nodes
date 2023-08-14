93e0900561f6f1e519f5b7e42e235a1d776a324e
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-14 16:15:32 -0400

    finalized region properties

--- TRANSFORMERS/IMAGE_PROCESSING/REGION_PROPERTIES/REGION_PROPERTIES.py
+++ TRANSFORMERS/IMAGE_PROCESSING/REGION_PROPERTIES/REGION_PROPERTIES.py
@@ -1,13 +1,15 @@
 from skimage.draw import ellipse
-from skimage.measure import label, regionprops, regionprops_table
+from skimage.measure import label, regionprops, find_contours
 from skimage.transform import rotate
-from flojoy import flojoy, Plotly, run_in_venv, Grayscale, Image
+from flojoy import flojoy, Plotly, run_in_venv, Image
 import plotly.express as px
+import plotly.graph_objects as go
+
 import numpy as np
 from typing import Optional
 import math 
 from nodes.VISUALIZERS.template import plot_layout
-from PIL import ImageFilter, Image as PILImage
+from PIL import Image as PILImage
 
 @flojoy
 @run_in_venv(pip_dependencies=["scikit-image==0.21.0"])
@@ -33,24 +35,61 @@ def REGION_PROPERTIES(default: Optional[Image] = None) -> Plotly:
         image = rotate(image, angle=15, order=0)
         rr, cc = ellipse(100, 100, 60, 50)
         image[rr, cc] = 1
+        
+    rgb_image = np.zeros((600, 600, 3), dtype=np.uint8)
+    rgb_image[..., 0] = image * 255  # Red channel
+    rgb_image[..., 1] = image * 255  # Green channel
+    rgb_image[..., 2] = image * 255  # Blue channel
+    layout = plot_layout(title="IMAGE")
+    fig = px.imshow(img=rgb_image)
+    fig.layout = layout
 
-    label_img = label(image)
-    regions = regionprops(label_img)
-    for props in regions:
+    labels = label(image)
+    rprops = regionprops(labels,image)
+    properties = ['area', 'eccentricity', 'perimeter','centroid',
+                  'orientation','axis_major_length', 'axis_minor_length']
+
+    for props in rprops:
         y0, x0 = props.centroid
         orientation = props.orientation
         x1 = x0 + math.cos(orientation) * 0.5 * props.axis_minor_length
         y1 = y0 - math.sin(orientation) * 0.5 * props.axis_minor_length
         x2 = x0 - math.sin(orientation) * 0.5 * props.axis_major_length
         y2 = y0 - math.cos(orientation) * 0.5 * props.axis_major_length
-
-
-    layout = plot_layout(title="IMAGE")
-    fig = px.imshow(img=image)
-    fig.layout = layout
     
-    # ax.plot((x0, x1), (y0, y1), '-r', linewidth=2.5)
-    # ax.plot((x0, x2), (y0, y2), '-r', linewidth=2.5)
-    # ax.plot(x0, y0, '.g', markersize=15)
+        line_trace1 = go.Scatter(x=[x0, x1], y=[y0, y1], mode='lines', line=dict(color='red', width=2.5), showlegend=False)
+        line_trace2 = go.Scatter(x=[x0, x2], y=[y0, y2], mode='lines', line=dict(color='red', width=2.5), showlegend=False)
+        marker_trace = go.Scatter(x=[x0], y=[y0], mode='markers', marker=dict(color='green', size=15), showlegend=False)
 
-    return Plotly(fig=fig)#Image(r=255*image, b=255*image, g=255*image)
+        fig.add_trace(line_trace1)
+        fig.add_trace(line_trace2)
+        fig.add_trace(marker_trace)
+
+        minr, minc, maxr, maxc = props.bbox
+        bx = [minc, maxc, maxc, minc, minc]
+        by = [minr, minr, maxr, maxr, minr]
+        
+        bounding_box_trace = go.Scatter(x=bx, y=by, mode='lines', line=dict(color='blue', width=2), showlegend=False)
+        fig.add_trace(bounding_box_trace)
+
+
+    for index in range(labels.max()):
+        label_i = rprops[index].label
+        contour = find_contours(labels == label_i, 0.5)[0]
+        y, x = contour.T
+        hoverinfo = ''
+        for prop_name in properties:
+            val = getattr(rprops[index], prop_name)
+            if type(val) == tuple:
+                line = [f'<b>{prop_name}: {v:.2f}</b><br>' for v in val]
+                hoverinfo += ",".join(line)
+            else:
+                hoverinfo += f'<b>{prop_name}: {val:.2f}</b><br>'
+        fig.add_trace(go.Scatter(
+            x=x, y=y, name=label_i,
+            mode='lines', fill='toself', showlegend=False,
+            hovertemplate=hoverinfo, hoveron='points+fills'))
+
+    fig.update_xaxes(range=[0, 600])
+    fig.update_yaxes(range=[0, 600])
+    return Plotly(fig=fig)
