efc95bda53e10ac78c785b27c885007b8d695529
Joey Yu <joey@itsjoeoui.com>
2023-08-15 16:20:31 -0400

    Add doc strings

--- LOADERS/CLOUD_DATABASE/FLOJOY_CLOUD_UPLOAD/FLOJOY_CLOUD_UPLOAD.py
+++ LOADERS/CLOUD_DATABASE/FLOJOY_CLOUD_UPLOAD/FLOJOY_CLOUD_UPLOAD.py
@@ -13,8 +13,19 @@ FLOJOY_CLOUD_URI: str = os.environ.get("FRONTIER_URI") or "https://cloud.flojoy.
 def FLOJOY_CLOUD_UPLOAD(
     default: DataContainer,
     measurement_id: Optional[str] = None,
-    dc_id: Optional[str] = None,
 ) -> DataContainer:
+    """The FLOJOY_CLOUD_UPLOAD node takes a DataContainer as input and uploads that to Flojoy Cloud.
+
+    Parameters
+    ----------
+    measurement_id: The measurement id of the data to be uploaded to Flojoy Cloud. If not provided, a new measurement will be created.
+
+    Returns
+    -------
+    DataContainer
+        The input DataContainer will be returned as it is
+    """
+
     api_key = get_env_var("FLOJOY_CLOUD_KEY")
 
     if api_key is None:
@@ -47,23 +58,4 @@ def FLOJOY_CLOUD_UPLOAD(
         if not (resp.status_code >= 200 and resp.status_code < 300):
             raise Exception(str(resp.json()["error"]))
 
-        return default
-
-    else:
-        # If there is nothing connected to the node, then it will download from cloud
-        if dc_id is None:
-            raise Exception("A data container ID is required for downstreaming")
-
-        resp = requests.get(
-            f"{FLOJOY_CLOUD_URI}/api/v1/dcs/{dc_id}",
-            headers={"api_key": api_key},
-        )
-        print(resp.json())
-        # TODO: now it only supports x and y
-        if not (resp.status_code == 200 or resp.status_code == 201):
-            raise Exception(resp.json()["error"])
-
-        return DataContainer(
-            x=resp.json()["data"][0]["dataContainer"]["x"],
-            y=resp.json()["data"][0]["dataContainer"]["y"],
-        )
+    return default
