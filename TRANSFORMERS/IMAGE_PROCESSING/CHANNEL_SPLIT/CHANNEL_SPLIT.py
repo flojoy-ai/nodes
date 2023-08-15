df673381b3f92270b1d50af70b5cf2b800c31836
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-15 14:00:13 -0400

    added type tolerance + docs

--- TRANSFORMERS/IMAGE_PROCESSING/CHANNEL_SPLIT/CHANNEL_SPLIT.py
+++ TRANSFORMERS/IMAGE_PROCESSING/CHANNEL_SPLIT/CHANNEL_SPLIT.py
@@ -1,8 +1,14 @@
-from flojoy import flojoy, Image
+from flojoy import flojoy, Image, Matrix
 from typing import TypedDict
 import numpy as np
 
-class CHANNEL_OUTPUTS(TypedDict):
+class IMAGE_CHANNEL_OUTPUTS(TypedDict):
+    r: Image
+    g: Image
+    b: Image 
+    a: Image
+
+class NUMPY_CHANNEL_OUTPUTS(TypedDict):
     r: Image
     g: Image
     b: Image 
@@ -10,53 +16,74 @@ class CHANNEL_OUTPUTS(TypedDict):
 
 
 @flojoy
-def CHANNEL_SPLIT(default: Image) -> CHANNEL_OUTPUTS:
+def CHANNEL_SPLIT(default: Image | Matrix) -> IMAGE_CHANNEL_OUTPUTS | NUMPY_CHANNEL_OUTPUTS:
     """
     The CHANNEL_SPLIT node returns the rgba channels of an image 
-    into 4 separate images for direct visualization
+    into 4 separate images for direct visualization. While the notion
+    of "splitting an image into RGBA channels" is inherently tied to coloured
+    pictures, this function will attempt to make sense of multiple input types.
+
+    Should the input be of type `Image`, then the function will return the RGBA channels.
+    Should the input be of type `Matrix`, meaning ideally a 3D `numpy` array
+    of size (L, M, 3) or (L, M, 4), then the function will return each 'channel' respectively.
+
 
     Returns
     -------
-    CHANNEL_OUTPUTS
+    IMAGE_CHANNEL_OUTPUTS
         The images, each channel itself is rendered with alpha=1, except
         the alpha channel itself.
     """
-
-    r = default.r
-    g = default.g
-    b = default.b
-    a = default.a
-    
     try:
+        if isinstance(default, Image):
+            r = default.r
+            g = default.g
+            b = default.b
+            a = default.a
+            zeros = np.zeros(b.shape, np.uint8)
+            ones = 255*np.ones(b.shape, np.uint8)
 
-        zeros = np.zeros(b.shape, np.uint8)
-        ones = 255*np.ones(b.shape, np.uint8)
-
-        return CHANNEL_OUTPUTS(
-            r=Image(
-                r=r,
-                g=zeros,
-                b=zeros,
-                a=ones,
-            ), 
-            g=Image(
-                r=zeros,
+            return IMAGE_CHANNEL_OUTPUTS(
+                r=Image(
+                    r=r,
+                    g=zeros,
+                    b=zeros,
+                    a=ones,
+                ), 
+                g=Image(
+                    r=zeros,
+                    g=g,
+                    b=zeros,
+                    a=ones,
+                ), 
+                b=Image(
+                    r=zeros,
+                    g=zeros,
+                    b=b,
+                    a=ones,
+                ), 
+                a=Image(
+                    r=zeros,
+                    g=zeros,
+                    b=zeros,
+                    a=a,
+                ), 
+            )
+        elif isinstance(default, Matrix):
+            r = default.m[...,0]
+            g = default.m[...,1]
+            b = default.m[...,2]
+            a = None if default.m.shape[-1] == 3 else default.m[...,3]
+            
+            if default.m.shape[-1] != 3 or default.m.shape[-1] !=4:
+                raise IndexError("Input array is not of sensible size to split channels")
+            return NUMPY_CHANNEL_OUTPUTS(
+                r = r,
                 g=g,
-                b=zeros,
-                a=ones,
-            ), 
-            b=Image(
-                r=zeros,
-                g=zeros,
-                b=b,
-                a=ones,
-            ), 
-            a=Image(
-                r=zeros,
-                g=zeros,
-                b=zeros,
-                a=a,
-            ), 
-        )
+                b=b, 
+                a=a
+            )
+        else:
+            raise TypeError("Unexpected type of the input argument.")
     except Exception as e:
         raise e
