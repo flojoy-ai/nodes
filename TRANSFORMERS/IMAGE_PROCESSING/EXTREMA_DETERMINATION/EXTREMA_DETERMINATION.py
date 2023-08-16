b648c1e2de842c3f72660b6379f1e6208e004f1f
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-16 09:31:21 -0400

    finalized multiple algorithms for all DR and contrast

--- TRANSFORMERS/IMAGE_PROCESSING/EXTREMA_DETERMINATION/EXTREMA_DETERMINATION.py
+++ TRANSFORMERS/IMAGE_PROCESSING/EXTREMA_DETERMINATION/EXTREMA_DETERMINATION.py
@@ -7,6 +7,9 @@ from itertools import combinations
 
 import scipy.fft
 from scipy.fft import _pocketfft
+from scipy import spatial
+from scipy.signal import fftconvolve
+
 from os import cpu_count
 
 from warnings import catch_warnings, simplefilter
@@ -23,16 +26,18 @@ from skimage.draw import ellipse
 import skimage.filters as filters
 from skimage.measure import label, regionprops
 from skimage.morphology import binary_erosion, disk
-
+from skimage.feature import blob_log
 
 @flojoy(deps={"scikit-image":"0.21.0"})
 def EXTREMA_DETERMINATION(
     default: Image | Grayscale | Matrix,
-    mask : Optional[Grayscale | Matrix] = None,
+    image_mask : Optional[Grayscale | Matrix] = None,
     center: list[int] = None,
     min_dist: float = 0.0,
-    persistence_algorithm: bool = True,
+    algorithm: str = "log",
     prominence: float = 0.0,
+    k: float = 1.41,
+    sigma: float = 1.0
 ) -> Plotly:
 
     """
@@ -109,99 +114,162 @@ def EXTREMA_DETERMINATION(
         
     # This now produces an (M, N) array that we can then process! The algorthims
     # have no specificity on input data type unlike the `REGION_PROPERTIES` node.
-    if mask is None:
+    if image_mask is None:
         mask = np.ones(image.shape)
     else:
+        mask = image_mask.m
         if mask.shape != image.shape:
             raise IndexError("Provided mask is not the same shape as the input image.")
+    blob_mask = np.zeros(image.shape, dtype=np.uint8)
+    match algorithm:
+        case 'high_symmetry':
+            if center is None:
+                center = autocenter(im=image, mask=mask)
+                # Then we need to autodetermine the center of the iamge
+                # raise ValueError("For the crossed correlated mask algorithm, the center of the peaks \
+                #                   in the image must be specified")
+            im = np.array(image, copy=True, dtype=float)
+            im -= im.min()
 
-    if not persistence_algorithm:
-        if center is None:
-            center = autocenter(im=image, mask=mask)
-            # Then we need to autodetermine the center of the iamge
-            # raise ValueError("For the crossed correlated mask algorithm, the center of the peaks \
-            #                   in the image must be specified")
-        im = np.array(image, copy=True, dtype=float)
-        im -= im.min()
+            with catch_warnings():
+                simplefilter("ignore", category=RuntimeWarning)
+                im /= gaussian_filter(input=im, sigma=min(image.shape) / 20, truncate=2)
+            im = np.nan_to_num(im, copy=False)
 
-        with catch_warnings():
-            simplefilter("ignore", category=RuntimeWarning)
-            im /= gaussian_filter(input=im, sigma=min(image.shape) / 20, truncate=2)
-        im = np.nan_to_num(im, copy=False)
+            autocorr = np.abs(
+                cross_correlate_masked(arr1=im, arr2=im, m1=mask, m2=mask, mode="same")
+            )
+            autocorr = shift(
+                autocorr,
+                shift=np.asarray(center) - np.array(im.shape) / 2,
+                order=1,
+                mode="nearest",
+            )
+            laplacian = -1 * laplace(autocorr)
+            threshold = filters.threshold_triangle(laplacian)
+            regions = (laplacian > threshold) * mask
 
-        autocorr = np.abs(
-            cross_correlate_masked(arr1=im, arr2=im, m1=mask, m2=mask, mode="same")
-        )
-        autocorr = shift(
-            autocorr,
-            shift=np.asarray(center) - np.array(im.shape) / 2,
-            order=1,
-            mode="nearest",
-        )
-        laplacian = -1 * laplace(autocorr)
-        threshold = filters.threshold_triangle(laplacian)
-        regions = (laplacian > threshold) * mask
+            # To prevent noise from looking like actual peaks,
+            # we erode labels using a small selection area
+            regions = binary_erosion(regions, footprint=disk(2))
 
-        # To prevent noise from looking like actual peaks,
-        # we erode labels using a small selection area
-        regions = binary_erosion(regions, footprint=disk(2))
-
-        labels = label(regions, return_num=False)
-        props = regionprops(label_image=labels, intensity_image=im)
-        candidates = [
-            prop for prop in props if not np.any(np.isnan(prop.weighted_centroid))
-        ]
-        peaks = list()
-        for prop in candidates:
-            pos = np.asarray(prop.weighted_centroid)
-            if any((np.linalg.norm(peak - pos) < min_dist) for peak in peaks):
-                continue
-            else:
-                peaks.append(pos[::-1])
-        peaks = np.array([peaks]).reshape(-1,2) #now gives us the final array of peaks
-    else: # we use the persistence algorithm
-        g0 = Persistence(image).persistence
-        birth_death = list()
-        birth_death_indices = list()
-        persistencies = list()
-        candidates = list()
-        bd_threshold = 0.0
-        for i, homclass in enumerate(g0):
-            p_birth, bl, pers, p_death = homclass
-            persistencies.append(pers)
-            if pers <= bd_threshold:
-                continue
-            x, y = bl, bl - pers
-            birth_death.append([x, y])
-            birth_death_indices.append(i)
-        for i, homclass in enumerate(g0):
-            p_birth, bl, pers, p_death = homclass
-            if pers <= prominence:
-                continue
-            y, x = p_birth
-            candidates.append([x, y])
-        if min_dist > 0.0:
-            combos = combinations(candidates, 2)
-            points_to_remove = [
-                point2
-                for point1, point2 in combos
-                if np.linalg.norm(np.array(point1) - np.array(point2)) < min_dist
+            labels = label(regions, return_num=False)
+            props = regionprops(label_image=labels, intensity_image=im)
+            candidates = [
+                prop for prop in props if not np.any(np.isnan(prop.weighted_centroid))
             ]
-            candidates = [point for point in candidates if point not in points_to_remove]
-        peaks = np.array(candidates).reshape(-1, 2)
-        print(persistencies)
-        # remove peaks that are within the masked area
-        if mask.sum() != mask.shape[0] * mask.shape[1]:
-            peaks = np.array([p for p in candidates if mask[p[1], p[0]]]).reshape(-1,2)
+            peaks = list()
+            for prop in candidates:
+                pos = np.asarray(prop.weighted_centroid)
+                if any((np.linalg.norm(peak - pos) < min_dist) for peak in peaks):
+                    continue
+                else:
+                    peaks.append(pos[::-1])
+            peaks = np.array([peaks]).reshape(-1,2) #now gives us the final array of peaks
+        case 'persistence': # we use the persistence algorithm
+            g0 = Persistence(image).persistence
+            birth_death = list()
+            birth_death_indices = list()
+            persistencies = list()
+            candidates = list()
+            bd_threshold = 0.0
+            for i, homclass in enumerate(g0):
+                p_birth, bl, pers, p_death = homclass
+                persistencies.append(pers)
+                if pers <= bd_threshold:
+                    continue
+                x, y = bl, bl - pers
+                birth_death.append([x, y])
+                birth_death_indices.append(i)
+            for i, homclass in enumerate(g0):
+                p_birth, bl, pers, p_death = homclass
+                if pers <= prominence:
+                    continue
+                y, x = p_birth
+                candidates.append([x, y])
+            if min_dist > 0.0:
+                combos = combinations(candidates, 2)
+                points_to_remove = [
+                    point2
+                    for point1, point2 in combos
+                    if np.linalg.norm(np.array(point1) - np.array(point2)) < min_dist
+                ]
+                candidates = [point for point in candidates if point not in points_to_remove]
+            peaks = np.array(candidates).reshape(-1, 2)
+            # remove peaks that are within the masked area
+            if mask.sum() != mask.shape[0] * mask.shape[1]:
+                peaks = np.array([p for p in candidates if mask[p[1], p[0]]]).reshape(-1,2)
+        case 'log':
+            # This is the most expensive algorithm!! 
+            # Use only when dire!
 
+            # Step 1: we need to define the lapacian of a gaussian for a given sigma
+            def laplacian_of_gaussian(sigma: float): # first define the laplacian of a gaussian filter
+                #window size 
+                n = np.ceil(sigma*6)
+                y,x = np.ogrid[-n//2:n//2+1,-n//2:n//2+1]
+                y_filter = np.exp(-(y**2/(2.*sigma*sigma)))
+                x_filter = np.exp(-(x**2/(2.*sigma*sigma)))
+                return (-(2*sigma**2) + (x*x + y*y) ) *  (x_filter*y_filter) * (1/(2*np.pi*sigma**4))
+            # Step 2: perform the convolution with the LOG filters for increasing powers of sigma
+            log_images = np.zeros((9, *image.shape)) #to store responses
+            with scipy.fft.set_backend(customFFTBackend):
+                for i in range(0,9):
+                    log_images[i,...] = np.square(
+                        fftconvolve(
+                        image, 
+                        laplacian_of_gaussian(sigma*np.power(k,i)) , 
+                        mode='same'
+                        )
+                    ) # squaring the response
+            # Step 3: detect the blobs
+            peaks_with_radius = list()
+            (h,w) = image.shape
+            for i in range(1,h):
+                for j in range(1,w):
+                    slice_img = log_images[:,i-1:i+2,j-1:j+2]
+                    result = np.amax(slice_img)
+                    #result_1 = np.amin(slice_img)
+                    if result >= prominence:
+                        z,x,y = np.unravel_index(slice_img.argmax(),slice_img.shape)
+                        peaks_with_radius.append((i+x-1,j+y-1,k**z*sigma))
+            candidates = list(set(peaks_with_radius))
+            candidates = np.array(candidates)
+            if min_dist > 0.0:
+                sigma = candidates[:, -1].max()
+                distance = 2 * sigma * np.sqrt(candidates.shape[1] - 1)
+                tree = spatial.cKDTree(candidates[:, :-1])
+                pairs = np.array(list(tree.query_pairs(distance)))
+                if len(pairs) == 0:
+                    return candidates
+                else:
+                    for (i, j) in pairs:
+                        blob1, blob2 = candidates[i], candidates[j]
+                        if np.linalg.norm(blob1[:-1] - np.array(blob2[:-1])) < min_dist:
+                            if blob1[-1] > blob2[-1]:
+                                blob2[-1] = 0
+                            else:
+                                blob1[-1] = 0
+                candidates = np.array([b for b in candidates if b[-1] > 0])
+            # Step 4: This method needs to be robust under the situation of contrast issues
+            # To get around this, we now pass these blobs to the standard blob detection 
+            # algorithms to get the centroid of each contour now that we've identified
+            # regions of interest
+            for blob in candidates:
+                y, x, r= blob
+                rr, cc = ellipse(x, y, r/2, r/2, shape=blob_mask.shape)
+                blob_mask[rr, cc] = 1
+            labels = label(blob_mask)
+            rprops = regionprops(label_image = labels, intensity_image = blob_mask)
+            peaks = list()
+            for region in [prop for prop in rprops if not np.any(np.isnan(prop.weighted_centroid))]:
+                peaks.append(region.weighted_centroid)
+            peaks = np.array(peaks).reshape(-1, 2)
     # Congratulations! We now have an (N,2) array of the peaks in the image;
     # let's visualize it!
     # Right now we have a greyscale image, let's create a version
     # that's black and white so we can render it. First, scale image to
     # range 0-255.
-    image -= image.min()
-    image = image / float(image.max())
-    image *= 255
 
     rgb_image = np.zeros((*image.shape, 3), dtype=np.uint8) #only generated for plotting
     rgb_image[..., 0] = image * 255  # Red channel
