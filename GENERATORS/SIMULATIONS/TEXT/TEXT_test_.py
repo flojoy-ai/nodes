2ec8dd8ce49f02c84fc7610e363a42be3c66ab45
Joey Yu <joey@itsjoeoui.com>
2023-08-17 22:43:06 -0400

    fix text test

--- GENERATORS/SIMULATIONS/TEXT/TEXT_test_.py
+++ GENERATORS/SIMULATIONS/TEXT/TEXT_test_.py
@@ -1,8 +1,5 @@
-import numpy as np
-
-
 def test_TEXT(mock_flojoy_decorator):
     import TEXT
 
     res = TEXT.TEXT(value="Hello World!")
-    assert np.equal("Hello World!", res.text_blob)
+    assert "Hello World!" == res.text_blob
