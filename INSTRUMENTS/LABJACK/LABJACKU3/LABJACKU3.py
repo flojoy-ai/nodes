ab448dd60c84904bec60035f853406323afa27ca
angelchauffray <ac@enclose.solar>
2023-08-18 14:26:23 -0400

    reformat files

--- INSTRUMENTS/LABJACK/LABJACKU3/LABJACKU3.py
+++ INSTRUMENTS/LABJACK/LABJACKU3/LABJACKU3.py
@@ -1,6 +1,8 @@
 from flojoy import flojoy, OrderedPair
 from typing import Optional
-from labjackpython import u3  # Import the library from LabJackPython in order to use our U3-LV device
+from labjackpython import (
+    u3,
+)  # Import the library from LabJackPython in order to use our U3-LV device
 
 
 @flojoy(deps={"labjackpython": "2.1.0"})
