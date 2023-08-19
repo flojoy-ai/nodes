4ea6c203b89ccb9af9d9c7c49b72fff180bc7023
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-19 10:05:07 -0400

    blackened

--- TRANSFORMERS/IMAGE_PROCESSING/CHANNEL_SPLIT/CHANNEL_SPLIT_test_.py
+++ TRANSFORMERS/IMAGE_PROCESSING/CHANNEL_SPLIT/CHANNEL_SPLIT_test_.py
@@ -12,13 +12,13 @@ def test_CHANNEL_SPLIT(mock_flojoy_decorator):
 
     input = Image(r=red, g=green, b=blue, a=alpha)
     res = CHANNEL_SPLIT.CHANNEL_SPLIT(input)
-    r = res['r'].r
-    g = res['g'].g
-    b = res['b'].b
-    a = res['a'].a
+    r = res["r"].r
+    g = res["g"].g
+    b = res["b"].b
+    a = res["a"].a
 
     # Assert returns image channels are matching the input channel
-    assert np.abs(red-r).sum() < 1e-1
-    assert np.abs(green-g).sum() < 1e-1
-    assert np.abs(blue-b).sum() < 1e-1
-    assert np.abs(alpha-a).sum() < 1e-1
+    assert np.abs(red - r).sum() < 1e-1
+    assert np.abs(green - g).sum() < 1e-1
+    assert np.abs(blue - b).sum() < 1e-1
+    assert np.abs(alpha - a).sum() < 1e-1
