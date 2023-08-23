81bbdd6b649e8bd4e3aed6667df47cd1293a6f27
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-23 11:17:40 -0400

    tweaked batch processor to serialize correctly

--- LOADERS/LOCAL_FILE_SYSTEM/BATCH_PROCESSOR/BATCH_PROCESSOR.py
+++ LOADERS/LOCAL_FILE_SYSTEM/BATCH_PROCESSOR/BATCH_PROCESSOR.py
@@ -9,14 +9,14 @@ memory_key = "batch-processor-info"
 def get_fnames(d, p):
     return [file for file in glob.glob(d+'/'+p, recursive=True)]
 
-@flojoy
+@flojoy(inject_node_metadata=True)
 def BATCH_PROCESSOR(
     current_iteration: Scalar,
     default_params: DefaultParams,
     directory_path: str,
     pattern: str = "",
     refresh: bool = True, 
-):
+) -> Vector:
     #=======================================================
     # At first iteration, we want to identify the initial 
     # sources to iterate over. Also at the first iteration,
@@ -41,25 +41,25 @@ def BATCH_PROCESSOR(
     # return popped string as Vec<str>
     #=======================================================
     node_id = default_params.node_id
-
+    curr_iter = int(current_iteration.c[0])
     # if iteration 1, pattern find, then write to SmallMemory
-    if current_iteration.c == 1: #loop index starts at 1, sigh
+    if curr_iter == 1: #loop index starts at 1, sigh
         files = get_fnames(directory_path, pattern if pattern else '*')
         SmallMemory().write_to_memory(
             node_id, 
             memory_key, 
             {
                 'node_id' : node_id,
-                'current_iteration' : current_iteration.c,
+                'current_iteration' : curr_iter,
                 'files' : files
             }
         )
     # if refresh, glob again, read from smallmemory, 
     # find difference, append difference to files in SmallMemory
     if refresh:
-        old_files = old_data['files']
         new_files = get_fnames(directory_path, pattern if pattern else '*')
         old_data: dict[str, Any] = SmallMemory().read_memory(node_id, memory_key) or {}
+        old_files = old_data['files']
         if old_data:
             difference = set(new_files).difference(set(old_files)) #designed to only catch the addition of files
             if difference:
@@ -69,20 +69,18 @@ def BATCH_PROCESSOR(
                     memory_key, 
                     {
                         'node_id' : node_id,
-                        'current_iteration' : current_iteration.c,
+                        'current_iteration' : curr_iter,
                         'files' : files+list(difference)
                     }
                 )
     # Now we read from SmallMemory and pop fname
     data: dict[str, Any] = SmallMemory().read_memory(node_id, memory_key) or {}
     fname = data['files'].pop(0)
-
     # Now write to SmallMemory for the next iteration
-    data['current_iteration'] = current_iteration.c
+    data['current_iteration'] = curr_iter
     SmallMemory().write_to_memory(node_id,memory_key, data)
-
     #And return the current fname
     return Vector(
-        v=np.array(fname)
+        v=np.array(fname)[...,np.newaxis]
     )
 
