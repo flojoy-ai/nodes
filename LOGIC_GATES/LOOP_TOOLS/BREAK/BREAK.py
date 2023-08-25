b4115b338fb770932b1178ad791b2c7441be09e2
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-25 10:51:26 -0400

    shallow copy for safety

--- LOGIC_GATES/LOOP_TOOLS/BREAK/BREAK.py
+++ LOGIC_GATES/LOOP_TOOLS/BREAK/BREAK.py
@@ -1,11 +1,7 @@
 from flojoy import flojoy, DataContainer, SmallMemory, NodeReference
 from typing import Optional, Any
-import json
-
 
 memory_key = "loop-info"
-
-
 
 @flojoy
 def BREAK(
@@ -13,7 +9,8 @@ def BREAK(
     default: Optional[DataContainer] = None,
 ) -> DataContainer:
     # this is the loop ID we want to break
-    data: dict[str, Any] = SmallMemory().read_memory(referred_node.ref, memory_key) 
+    original_data: dict[str, Any] = SmallMemory().read_memory(referred_node.ref, memory_key) 
+    data = original_data.copy()
     data['num_loop'] = 1
     data['current_iteration'] = 1
     data['is_finished'] = True
