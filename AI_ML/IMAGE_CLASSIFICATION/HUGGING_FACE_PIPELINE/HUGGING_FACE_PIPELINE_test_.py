bbdbf163c4123ff960dc2fc323bcf3015c9411ed
Julien Jerphanion <git@jjerphan.xyz>
2023-08-28 15:22:06 +0200

    tests: Test against several models and revisions
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- AI_ML/IMAGE_CLASSIFICATION/HUGGING_FACE_PIPELINE/HUGGING_FACE_PIPELINE_test_.py
+++ AI_ML/IMAGE_CLASSIFICATION/HUGGING_FACE_PIPELINE/HUGGING_FACE_PIPELINE_test_.py
@@ -1,10 +1,11 @@
 import os
 import pytest
 
-from PIL import Image as PIL_Image
 import numpy as np
+import pandas as pd
 
 from flojoy import Image, DataFrame
+from PIL import Image as PIL_Image
 
 
 @pytest.fixture
@@ -19,12 +20,13 @@ def ada_lovelace_array_rgb():
 
 
 @pytest.mark.slow
-def test_HUGGING_FACE_PIPELINE(
+def test_HUGGING_FACE_PIPELINE_default(
     mock_flojoy_decorator,
     mock_flojoy_venv_cache_directory,
     cleanup_flojoy_cache_fixture,
     ada_lovelace_array_rgb,
 ):
+    """HUGGING_FACE_PIPELINE functional test for the example application."""
     from HUGGING_FACE_PIPELINE import HUGGING_FACE_PIPELINE
 
     input_image = Image(
@@ -42,7 +44,43 @@ def test_HUGGING_FACE_PIPELINE(
 
     assert isinstance(df_classification_confidence_scores, DataFrame)
 
-    first_class_confidence_scores = classification_confidence_scores.iloc[0, :]
+    first_class_confidence_scores = df_classification_confidence_scores.m.iloc[0, :]
 
     assert first_class_confidence_scores["label"] == "overskirt"
     assert first_class_confidence_scores["score"] > 0.725
+
+
+@pytest.mark.parametrize(
+    "model, revision",
+    (
+        ("google/vit-base-patch16-224", "2ddc9d4"),
+        ("microsoft/beit-base-patch16-224-pt22k-ft22k", "9da3011"),
+        ("google/mobilenet_v1_0.75_192", "56dde11"),
+    ),
+)
+def test_HUGGING_FACE_PIPELINE_common_model_and_revisions(
+    mock_flojoy_decorator,
+    mock_flojoy_venv_cache_directory,
+    cleanup_flojoy_cache_fixture,
+    ada_lovelace_array_rgb,
+    model,
+    revision,
+):
+    """HUGGING_FACE_PIPELINE must work with a variety of models and revisions."""
+    from HUGGING_FACE_PIPELINE import HUGGING_FACE_PIPELINE
+
+    input_image = Image(
+        r=ada_lovelace_array_rgb[:, :, 0],
+        g=ada_lovelace_array_rgb[:, :, 1],
+        b=ada_lovelace_array_rgb[:, :, 2],
+        a=None,
+    )
+
+    df_classification_confidence_scores = HUGGING_FACE_PIPELINE(
+        default=input_image,
+        model=model,
+        revision=revision,
+    )
+
+    assert isinstance(df_classification_confidence_scores, DataFrame)
+    assert isinstance(df_classification_confidence_scores.m, pd.DataFrame)
