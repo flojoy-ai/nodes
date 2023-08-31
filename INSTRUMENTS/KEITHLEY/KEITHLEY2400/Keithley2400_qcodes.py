20807bd89d1188f5412eea2c6fe02daec6283af0
angelchauffray <ac@enclose.solar>
2023-08-31 17:13:57 -0400

    created script for Keithley2400 qcode nodes

---
+++ INSTRUMENTS/KEITHLEY/KEITHLEY2400/Keithley2400_qcodes.py
@@ -0,0 +1,52 @@
+from qcodes.instrument.visa import VisaInstrument
+from qcodes.instrument_drivers.tektronix.Keithley_2400 import Keithley2400
+
+@flojoy(deps={"qcodes": "3.5"})
+def KEITHLEY2400(
+    init_container: NodeInitContainer, default: OrderedPair | Vector
+) -> OrderedPair:
+
+# Create an instance of the Keithley2400 instrument
+keithley = Keithley2400("keithley", "TCPIP0::192.168.1.1::INSTR")
+
+# Connect to the instrument
+keithley.connect()
+
+# Now you can use the instrument to perform measurements and control the power supply
+# For example, you can set the voltage range
+keithley.rangev(10)  # Set the voltage range to 10V
+
+# You can also set the current range
+keithley.rangei(1)  # Set the current range to 1A
+
+# Set the voltage compliance
+keithley.compliancev(5)  # Set the voltage compliance to 5V
+
+# Set the current compliance
+keithley.compliancei(0.1)  # Set the current compliance to 0.1A
+
+# Set the output mode to voltage
+keithley.mode("VOLT")  # Set the mode to voltage
+
+# Set the sense mode to voltage
+keithley.sense("VOLT")  # Set the sense mode to voltage
+
+# Enable the output
+keithley.output("on")  # Turn on the output
+
+# Set the voltage level
+keithley.volt(2)  # Set the voltage level to 2V
+
+# Read the current
+current = keithley.curr()  # Read the current
+
+# Read the resistance
+resistance = keithley.resistance()  # Read the resistance
+
+# Disable the output
+keithley.output("off")  # Turn off the output
+
+# Disconnect from the instrument
+keithley.disconnect()
+
+return OrderedPair(x=voltages, y=currents_neg)
