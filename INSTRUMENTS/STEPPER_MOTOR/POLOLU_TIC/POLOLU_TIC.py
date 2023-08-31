ee85d7485029da4c5c92784cc9427ccd2a0f3773
tobin ngo <ngo.tobin@gmail.com>
2023-08-31 10:33:42 -0400

    initial commit

---
+++ INSTRUMENTS/STEPPER_MOTOR/POLOLU_TIC/POLOLU_TIC.py
@@ -0,0 +1,56 @@
+from flojoy import flojoy, OrderedPair
+from ticlib import ticUSB
+
+tic = ticUSB()
+
+@flojoy(deps={"ticlib": "0.2.2", "pyusb":"1.2.1"})
+def POLOLU_TIC(
+    initial_position: int = 0,
+    target_position: int = 0,
+) -> OrderedPair:
+
+    """
+    The POLOLU_TIC node allows you to set starting and target positions of a stepper
+    motor compatible with the Pololu Tic T500, T249, T825, T834 or T36v4 stepper drivers. It
+    returns time elapsed until target position is reached. 
+    
+    Inputs  
+    ------  
+    initial_postion
+        sets tapper motor starting postion 
+    target_position
+        sets end of movement node
+
+    Parameters  
+    ----------   
+    initial_positiion : int
+        current stepper motor position  
+    target_position : int
+        target stepper motor position
+    
+    Returns  
+    -------  
+    OrderedPair 
+        x: current stepper motor position  
+        y: the amount of time in milliseconds until target position is reached
+    
+    """
+    tic.energize()
+    tic.exit_safe_start()
+
+    tic.halt_and_set_position(initial_position)
+    tic.set_current_limit(32)
+    tic.set_decay_mode(0)
+    tic.set_max_acceleration(40000)
+    tic.set_max_deceleration(0)
+    tic.set_max_speed(200000)
+    tic.set_starting_speed(180000)
+    tic.set_step_mode(0)
+    tic.set_target_position(target_position)
+
+    time_since_last_step = tic.get_time_since_last_step() / 1000
+
+    tic.deenergize()
+    tic.enter_safe_start() 
+
+    return OrderedPair(x=initial_position, y=time_since_last_step)
