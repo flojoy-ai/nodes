30b56abbefa2a6ebc0154abc7600c6cc24c32d7a
Julien Jerphanion <git@jjerphan.xyz>
2023-09-06 09:08:10 +0200

    Format with black
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- LOADERS/REMOTE_FILE_SYSTEM/REMOTE_FILE/REMOTE_FILE_test_.py
+++ LOADERS/REMOTE_FILE_SYSTEM/REMOTE_FILE/REMOTE_FILE_test_.py
@@ -4,17 +4,25 @@ from flojoy import Image, DataFrame, Grayscale
 
 FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL = "https://raw.githubusercontent.com/flojoy-ai/nodes/22908c9fbee6b923b10d36091be31c05c9b74e03/LOADERS/LOCAL_FILE_SYSTEM/LOCAL_FILE/assets"  # noqa: E501
 
-@pytest.mark.parametrize("file_type, output_type, file_url", [
-    ("Image", Image, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/astronaut.png"),
-    ("Grayscale", Grayscale, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/camera.png"),
-    ("CSV", DataFrame, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/iris_test.csv"),
-    ("JSON", DataFrame, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/employees.json"),
-    ("XML", DataFrame, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/menu.xml"),
-])
-def test_REMOTE_FILE_valid_usage(mock_flojoy_decorator, file_type, output_type, file_url):
+
+@pytest.mark.parametrize(
+    "file_type, output_type, file_url",
+    [
+        ("Image", Image, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/astronaut.png"),
+        ("Grayscale", Grayscale, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/camera.png"),
+        ("CSV", DataFrame, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/iris_test.csv"),
+        ("JSON", DataFrame, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/employees.json"),
+        ("XML", DataFrame, f"{FLOJOY_LOCAL_FILE_SYSTEM_ASSETS_URL}/menu.xml"),
+    ],
+)
+def test_REMOTE_FILE_valid_usage(
+    mock_flojoy_decorator, file_type, output_type, file_url
+):
     import REMOTE_FILE
+
     output = REMOTE_FILE.REMOTE_FILE(file_url=file_url, file_type=file_type)
     assert isinstance(output, output_type)
+
 
 @pytest.mark.parametrize(
     "file_url",
