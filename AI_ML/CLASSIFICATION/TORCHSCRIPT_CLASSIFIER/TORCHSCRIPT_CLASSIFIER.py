5584e872512e31e0709689eee039f309a18036ee
Julien Jerphanion <git@jjerphan.xyz>
2023-09-06 10:14:55 +0200

    Loosen dependencies for integration within studio
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- AI_ML/CLASSIFICATION/TORCHSCRIPT_CLASSIFIER/TORCHSCRIPT_CLASSIFIER.py
+++ AI_ML/CLASSIFICATION/TORCHSCRIPT_CLASSIFIER/TORCHSCRIPT_CLASSIFIER.py
@@ -6,8 +6,8 @@ from flojoy import flojoy, run_in_venv, Image, DataFrame
     pip_dependencies=[
         "torch==2.0.1",
         "torchvision==0.15.2",
-        "numpy==1.24.3",
-        "Pillow==9.5.0",
+        "numpy",
+        "Pillow",
     ]
 )
 def TORCHSCRIPT_CLASSIFIER(
