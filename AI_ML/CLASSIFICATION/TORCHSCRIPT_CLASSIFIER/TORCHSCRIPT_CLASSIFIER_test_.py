b022c9646e4d8bc9809d723cfd42915acb765c99
Reda Oulbacha <oulbacha.reda@gmail.com>
2023-09-07 12:39:15 -0400

    pull mobilenetv3 from the net

--- AI_ML/CLASSIFICATION/TORCHSCRIPT_CLASSIFIER/TORCHSCRIPT_CLASSIFIER_test_.py
+++ AI_ML/CLASSIFICATION/TORCHSCRIPT_CLASSIFIER/TORCHSCRIPT_CLASSIFIER_test_.py
@@ -1,18 +1,44 @@
 import pytest
 import os
+import tempfile
 import pandas as pd
 import numpy as np
 import PIL
-from flojoy import Image, DataFrame
+from flojoy import run_in_venv, Image, DataFrame
 
 
 @pytest.fixture
 def torchscript_model_path():
-    return os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "assets",
-        "mbnet_v3_small.torchscript",
-    )
+    # Download and save a test model, this requires
+    # torch which is why we need to run this in a venv.
+    @run_in_venv(pip_dependencies=["torch~=2.0.1", "torchvision~=0.15.2"])
+    def _download_test_model(path: str):
+        import torch
+        import torchvision
+
+        class ModelWithTransform(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.model = torch.hub.load(
+                    "pytorch/vision:v0.15.2",
+                    "mobilenet_v3_small",
+                    pretrained=True,
+                    skip_validation=True, # This will save us from github rate limiting, https://github.com/pytorch/vision/issues/4156#issuecomment-939680999
+                )
+                self.model.eval()
+                self.transforms = torchvision.models.MobileNet_V3_Small_Weights.DEFAULT.transforms()
+
+            def forward(self, x):
+                return self.model(self.transforms(x))
+
+        model = ModelWithTransform()
+        scripted = torch.jit.script(model)
+        torch.jit.save(scripted, path)
+    
+    with tempfile.TemporaryDirectory() as tempdir:
+        model_path = os.path.join(tempdir, "mbnet_v3_small.torchscript")
+        _download_test_model(model_path)
+        yield model_path
 
 
 @pytest.fixture
