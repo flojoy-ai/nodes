4f5bdaacb246d5d694ac7644d1c89a30aef39c58
Youngsun Yoo <youngsun123q@gmail.com>
2023-09-09 19:52:52 -0400

    Completed text concat node

---
+++ TRANSFORMERS/TEXT_MANIPULATION/TEXT_CONCAT/TEXT_CONCAT.py
@@ -0,0 +1,45 @@
+from flojoy import TextBlob, flojoy
+from typing import Literal
+
+
+@flojoy
+def TEXT_CONCAT(
+    a: TextBlob,
+    b: TextBlob,
+    delimiter: Literal["space", "comma", "semicolon", "colon", "new line"] = "space",
+) -> TextBlob:
+    """The TEXT_CONCAT node takes in two TextBlob DataContainer type and cancatenates the text string.
+
+    Inputs
+    ------
+    a: TextBlob
+        The input text to be concatenated to input b
+
+    b: TextBlob
+        The input text to be concatenated to input a
+
+    Parameters
+    ----------
+    delimiter: "space" | "comma" | "semicolon" | "colon" | "new line", default="space"
+        Select the delimiter to place between two text.
+
+    Returns
+    -------
+    TextBlob
+       The text result from concatenation.
+    """
+
+    delim: str = ""
+    match delimiter:
+        case "space":
+            delim = " "
+        case "comma":
+            delim = ","
+        case "semicolon":
+            delim = ";"
+        case "colon":
+            delim = ":"
+        case "new line":
+            delim = "\n"
+
+    return TextBlob(text_blob=delim.join([a.text_blob, b.text_blob]))
