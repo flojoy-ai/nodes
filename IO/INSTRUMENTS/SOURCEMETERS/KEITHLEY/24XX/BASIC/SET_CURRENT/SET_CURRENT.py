72626589380c282b286f20721b772f2486b5564e
angelchauffray <ac@enclose.solar>
2023-09-13 16:44:00 -0400

    SET_V_MEAS_I nodes for Keithley 2400

--- IO/INSTRUMENTS/SOURCEMETERS/KEITHLEY/24XX/BASIC/SET_CURRENT/SET_CURRENT.py
+++ IO/INSTRUMENTS/SOURCEMETERS/KEITHLEY/24XX/BASIC/SET_CURRENT/SET_CURRENT.py
@@ -7,53 +7,26 @@ from qcodes.instrument_drivers.Keithley.Keithley_2400 import Keithley2400
 @flojoy(deps={"qcodes": "0.39.1", "pyvisa-py": "0.6.3", "pyserial": "3.5"})
 def SET_CURRENT(
     default: Optional[DataContainer] = None,
-    current: int = 2,
+    current: int = 1,
     voltage_range: int = 10,
     current_range: int = 4,
+    compliance_voltage: int = 10,
+    compliance_current: int = 1,
 ) -> OrderedPair:
     # Create an instance of the Keithley2400 instrument
     keithley = Keithley2400("keithley", "ASRL/dev/ttyUSB1::INSTR")
 
-    # Connect to the instrument
-    # keithley.reset()
+    keithley.rangev(voltage_range)  # Set the voltage range
+    keithley.rangei(current_range)  # Set the current range
 
-    # Now you can use the instrument to perform measurements and control the power supply
-    # For example, you can set the voltage range
-    # keithley.ask(":SOUR:FUNC:MODE VOLT\n")
-    keithley.rangev(voltage_range)  # Set the voltage range to 10V
+    keithley.compliancev(compliance_voltage)  # Set the voltage compliance
+    keithley.compliancei(compliance_current)  # Set the current compliance
 
-    # You can also set the current range
-
-    keithley.rangei(current_range)  # Set the current range to 1A
-
-    # Set the voltage compliance
-    keithley.compliancev(5)  # Set the voltage compliance to 5V
-
-    # Set the current compliance
-    keithley.compliancei(5)  # Set the current compliance to 5A
-
-    # Set the output mode to voltage
-    keithley.mode("CURR")  # Set the mode to voltage
-
-    # Set the sense mode to voltage
+    keithley.mode("CURR")  # Set the mode to Current
     keithley.sense("VOLT")  # Set the sense mode to voltage
 
-    # Set the voltage level
-    keithley.curr(current)  # Set the voltage level to 2V
-
-    # Imprimer le statut de la sortie de l'instrument
-    output_status = keithley.output()  # Lire le statut de la sortie
-    print(f"Statut de la sortie : {output_status}")
-
-    # Activer la sortie de l'instrument si nécessaire
-    if output_status != "on":
-        keithley.output("on")
-
-    # Lire le courant
-    voltage_measured = keithley.volt()  # Read the current
-
-    # Read the resistance
-    resistance = keithley.resistance()  # Read the resistance
+    keithley.curr(current)  # Set the current
+    voltage_measured = keithley.volt()  # Read the voltage
 
     # Disable the output
     keithley.output("off")  # Turn off the output
@@ -61,4 +34,4 @@ def SET_CURRENT(
     # Disconnect from the instrument
     keithley.disconnect()
 
-    return OrderedPair(resistance, voltage_measured)
+    return OrderedPair(current, voltage_measured)
--- IO/INSTRUMENTS/SOURCEMETERS/KEITHLEY/24XX/BASIC/SET_VOLTAGE/SET_V_MEAS_I.py
+++ IO/INSTRUMENTS/SOURCEMETERS/KEITHLEY/24XX/BASIC/SET_VOLTAGE/SET_V_MEAS_I.py
@@ -1,19 +1,23 @@
 from flojoy import flojoy, OrderedPair, Vector, node_initialization, NodeInitContainer
 import serial
+from typing import Optional
 
 
 @flojoy(deps={"pyserial": "3.5"})
-def IV_SWEEP(
-    init_container: NodeInitContainer, default: OrderedPair | Vector
+def SET_V_MEAS_I(
+    init_container: NodeInitContainer, default: Optional[OrderedPair] = None,
+    voltage: int = 1,
 ) -> OrderedPair:
-    """The IV_SWEEP node takes a IV curve measurement with a Keithley 2400 source meter, send voltages, and measures currents.
+    """The SET_V_MEAS_I allows you to control the Keithley 2400 source meter. Set a voltage, and measure the current coming from the device under test. 
 
     Parameters
     -----------
     comport : string
-         defines the serial communication port for the Keithley2400 source meter.
+         defines the serial communication port for the Keithley2400 sourcemeter.
     baudrate : float
          specifies the baud rate for the serial communication between the Keithley2400 and the computer
+    voltage : int 
+         Specifies the voltage sourced by the Keithley2400
     """
 
     ser = init_container.get()
@@ -28,35 +32,27 @@ def IV_SWEEP(
         b":SENS:CURR:PROT 1.05\n"
     )  # Current protection set at 1.05A (Keithley 2400)
 
-    match default:
-        case OrderedPair():
-            voltages = default.y
-        case Vector():
-            voltages = default.v
+    current: list[float] = []  # measured currents
 
-    currents_neg: list[float] = []  # measured currents
+    ser.write(b":SOUR:VOLT %f\n" % voltage)  # Source Tension (V)
+    ser.write(b":OUTP ON\n")  # Instrument output open
+    ser.write(b":INIT\n")  # Start measuring
+    ser.write(b":FETC?\n")  # Retrieve the measured values
 
-    for voltage in voltages:
-        ser.write(b":SOUR:VOLT %f\n" % voltage)  # Source Tension (V)
-        ser.write(b":OUTP ON\n")  # Instrument output open
-        ser.write(b":INIT\n")  # Start measuring
-        ser.write(b":FETC?\n")  # Retrieve the measured values
+    current_str: str = (ser.readline().decode(
+        "ascii").strip())  # Save answers in a string
+    voltage_current_values: str = current_str.split(",")  # Split the string
+    current = float(voltage_current_values[1])
 
-        current_str: str = (
-            ser.readline().decode("ascii").strip()
-        )  # Save answers in a string
-        voltage_current_values: str = current_str.split(",")  # Split the string
-        currents_neg.append(-float(voltage_current_values[1]))
-
-        ser.write(b":OUTP OFF\n")  # Close output from Instrument
+    ser.write(b":OUTP OFF\n")  # Close output from Instrument
 
     # Close Serial Communication
     ser.close()
 
-    return OrderedPair(x=voltages, y=currents_neg)
+    return OrderedPair(x=voltage, y=current)
 
 
-@node_initialization(for_node=IV_SWEEP)
+@node_initialization(for_node=SET_V_MEAS_I)
 def init(comport: str = "/dev/ttyUSB0", baudrate: float = 9600):
     ser: serial = serial.Serial()
 
