bbc2c74537ddd907be32be4cbbc9393b89856877
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-09-14 00:26:50 -0400

    Add I/O to doc comments for mecademic nodes that didn't previously include them, and improve doc details

--- INSTRUMENTS/MECADEMIC/HOME/HOME.py
+++ INSTRUMENTS/MECADEMIC/HOME/HOME.py
@@ -4,16 +4,21 @@ from flojoy import flojoy, DataContainer
 @flojoy(deps={"mecademicpy": "1.4.0"})
 def HOME(ConnHandle: DataContainer) -> DataContainer:
     """
-    The HOME node homes the robot arm.
-            
+    The HOME node homes the robot arm. This node is required to be run before any other robot arm movement. It is recommended to run this node immediately after "ACTIVATE".
+
+    Inputs
+    ------
+    ConnHandle
+        A handle to the robot arm object.
+
     Returns
     -------
     DataContainer
-        Extra: A handle to the activated robot arm object.
-        
+        Containing a handle to the robot arm object, which is now homed assuming no errors were encountered.
+
     """
     if not ConnHandle.extra.IsConnected():
         raise ValueError("Robot connection failed.")
-    
+
     ConnHandle.extra.Activate()
     return ConnHandle
--- INSTRUMENTS/MECADEMIC/MOVE_POSE/MOVE_POSE.py
+++ INSTRUMENTS/MECADEMIC/MOVE_POSE/MOVE_POSE.py
@@ -2,45 +2,47 @@ from flojoy import flojoy, node_initialization, NodeInitContainer, DataContainer
 from typing import Optional
 
 
-@flojoy
+@flojoy(deps={"mecademicpy": "1.4.0"})
 def MOVE_POSE(
-    ConnHandle: DataContainer,
-    init_container: NodeInitContainer
-) -> DataContainer:
-    """
-    The MOVE_POSE node linearly moves the robot's tool to an absolute Cartesian position.
-    
-    Inputs
-    ------
-    ConnHandle
-        A handle to the robot arm object.
-        
-    Returns
-    -------
-    ConnHandle
-        A handle to the robot arm object after it has been moved.
-        
-    """
-    input = init_container.get()
-    s, i = input.x, input.y
-
-    if not ConnHandle.extra.IsConnected():
-        raise ValueError("Robot connection failed.")
-
-    ConnHandle.extra.MovePose(x=s.x, y=s.y, z=s.z, alpha=i.a, beta=i.b, gamma=i.g)
-    return ConnHandle
-
-
-@node_initialization(for_node=MOVE_POSE)
-def init(
+    ConnHandle: DataContainer, # TODO: use explicit generic type for robot in the DataContainer IE DataContainer[MecademicRobotHandle]
     x: float,
     y: float,
     z: float,
     a: Optional[float],
     b: Optional[float],
     g: Optional[float],
-) -> OrderedPair:
-    return OrderedPair(
-        x=Surface(x=x, y=y, z=z),
-        y=Image(a=a, b=b, g=g)
-    )
+) -> DataContainer:
+    """
+    The MOVE_POSE node linearly moves the robot's tool to an absolute Cartesian position.
+
+    Inputs
+    ------
+    ConnHandle
+        A handle to the robot arm object.
+
+    Parameters
+    ------
+    x : float
+        The x coordinate of the position to move to
+    y : float
+        The y coordinate of the position to move to
+    z : float
+        The z coordinate of the position to move to
+    a : float, optional
+        The alpha coordinate (rotation in radians about the x axis) of the position to move to.
+    b : float, optional
+        The beta coordinate   (rotation in radians about the y axis) of the position to move to.
+    g : float, optional
+        The gamma coordinate (rotation in radians about the z axis) of the position to move to.
+
+    Returns
+    -------
+    ConnHandle
+        A handle to the robot arm object after it has been moved.
+
+    """
+    if not ConnHandle.extra.IsConnected():
+        raise ValueError("Robot connection failed.")
+
+    ConnHandle.extra.MovePose(x=x, y=y, z=z, alpha=a, beta=b, gamma=g)
+    return ConnHandle
