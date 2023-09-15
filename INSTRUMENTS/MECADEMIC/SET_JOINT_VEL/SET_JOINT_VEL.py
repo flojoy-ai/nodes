8e4ac5746caa242858e13d34f84cc28edfb2822e
Henri Lemoine <henri123lemoine@gmail.com>
2023-09-15 17:37:11 -0400

    reset SET_JOINT_VEL conn_handle type

--- INSTRUMENTS/MECADEMIC/SET_JOINT_VEL/SET_JOINT_VEL.py
+++ INSTRUMENTS/MECADEMIC/SET_JOINT_VEL/SET_JOINT_VEL.py
@@ -1,15 +1,15 @@
-from flojoy import flojoy, MecademicConnHandle
+from flojoy import flojoy, Bytes
 from PYTHON.utils.mecademic_utils import check_connection
 
 
 @flojoy(deps={"mecademicpy": "1.4.0"})
-def SET_JOINT_VEL(conn_handle: MecademicConnHandle, v: float) -> MecademicConnHandle:
+def SET_JOINT_VEL(conn_handle: Bytes, v: float) -> Bytes:
     """
     The SET_JOINT_VEL node sets the robot arm's angular velocity for its joints.
 
     Inputs
     ------
-    conn_handle : MecademicConnHandle
+    conn_handle : Bytes
         A handle to the robot arm object.
 
     Parameters
@@ -23,6 +23,6 @@ def SET_JOINT_VEL(conn_handle: MecademicConnHandle, v: float) -> MecademicConnHa
         A handle to the robot arm object after its joint velocity has been set.
 
     """
-    check_connection(conn_handle.robot)
-    conn_handle.robot.SetJointVel(v)
+    check_connection(conn_handle.b)
+    conn_handle.b.SetJointVel(v)
     return conn_handle
