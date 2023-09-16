3ee226e19098209bdea746a660e02883eb2d8b42
Jinwon Lee <jinwonlee0916@gmail.com>
2023-09-16 13:14:11 -0400

    REMOVE_DUPLICATES_VECTOR node

---
+++ TRANSFORMERS/VECTOR_MANIPULATION/REMOVE_DUPLICATES_VECTOR/REMOVE_DUPLICATES_VECTOR.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flojoy import flojoy, Vector
+
+@flojoy
+def REMOVE_DUPLICATES_VECTOR(
+    default: Vector,
+) -> Vector:
+    """The REMOVE_DUPLICATES_VECTOR node returns a vector with only unique elements
+
+    Inputs
+    ------
+    v : Vector
+        The input vector
+
+    Returns
+    -------
+    Vector
+        Unique input vector
+    """
+
+    return Vector(v=np.unique(default.v))
