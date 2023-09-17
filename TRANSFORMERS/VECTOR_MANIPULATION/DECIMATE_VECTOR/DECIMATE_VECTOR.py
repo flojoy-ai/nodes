51e049a2b8a9c1d3156050a81516d2c6c0f30721
Jinwon Lee <jinwonlee0916@gmail.com>
2023-09-17 14:20:13 -0400

    DECIMATE_VECTOR node

---
+++ TRANSFORMERS/VECTOR_MANIPULATION/DECIMATE_VECTOR/DECIMATE_VECTOR.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import signal
+from flojoy import flojoy, Vector
+
+
+@flojoy
+def DECIMATE_VECTOR(
+    default: Vector,
+    factor: int = 1,
+) -> Vector:
+    """The DECIMATE_VECTOR node returns the input vector where
+    the elements are in the reverse order
+
+    Inputs
+    ------
+    default : Vector
+        The input vector
+
+    Parameters
+    ----------
+    factor : int
+        Decimate factor which determines how many elements will be skipped 
+        between each selected element in the output vector
+
+    Returns
+    -------
+    Vector
+        Decimated vector
+    """
+    
+    return Vector(v=default.v[::factor])
