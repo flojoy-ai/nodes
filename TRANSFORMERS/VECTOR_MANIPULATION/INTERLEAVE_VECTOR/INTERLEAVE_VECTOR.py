a644c779b060fb4376c3c409ef5a79c2b9294a69
Jinwon Lee <jinwonlee0916@gmail.com>
2023-09-21 20:14:22 -0400

    interleave node

---
+++ TRANSFORMERS/VECTOR_MANIPULATION/INTERLEAVE_VECTOR/INTERLEAVE_VECTOR.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flojoy import flojoy, Vector
+
+
+@flojoy
+def INTERLEAVE_VECTOR(
+    default: Vector,
+    a: list[Vector],
+) -> Vector:
+    """The INTERLEAVE_VECTOR node combine multiple vectors
+    into a single vector type by interleaving their elements
+
+    Inputs
+    ------
+    default : Vector
+        The input vector
+
+    Returns
+    -------
+    Vector
+        Decimated vector
+    """
+    interleavedVectors = [default.v]
+
+    for i in range(len(a)):
+        interleavedVectors = interleavedVectors + [a[i].v] 
+
+    interleavedVector = np.stack(interleavedVectors)
+    interleavedVector = interleavedVector.T.flatten()
+
+    return Vector(v=interleavedVector)
