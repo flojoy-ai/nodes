532b564bad3f1ffc33f2c32fa95dc429540702e1
Joey Yu <joey@jyu.dev>
2023-10-05 15:39:10 -0400

    silent linter :(

--- GENERATORS/SAMPLE_DATASETS/SCIKIT_LEARN_DATASET/SCIKIT_LEARN_DATASET_test_.py
+++ GENERATORS/SAMPLE_DATASETS/SCIKIT_LEARN_DATASET/SCIKIT_LEARN_DATASET_test_.py
@@ -2,19 +2,17 @@ from flojoy import DataFrame
 from sklearn.datasets import load_diabetes, load_iris
 
 
-# Tests that the function returns the expected DataFrame when called with the default dataset_key parameter
 def test_load_iris(mock_flojoy_decorator):
     import SCIKIT_LEARN_DATASET
 
-    result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_name="iris")
+    result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_name="iris")  # type: ignore
     assert isinstance(result, DataFrame)
     assert result.m.equals(load_iris(as_frame=True, return_X_y=True)[0])
 
 
-# Tests that the function returns the expected DataFrame when called with the 'cars' dataset_key parameter
 def test_load_diabetes(mock_flojoy_decorator):
     import SCIKIT_LEARN_DATASET
 
-    result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_name="diabetes")
+    result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_name="diabetes")  # type: ignore
     assert isinstance(result, DataFrame)
     assert result.m.equals(load_diabetes(as_frame=True, return_X_y=True)[0])
