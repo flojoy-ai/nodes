71f3bdaf260dd5941497e21c0dcec64e04ca655f
Joey Yu <joey@jyu.dev>
2023-10-05 15:37:54 -0400

    Done

--- GENERATORS/SAMPLE_DATASETS/SCIKIT_LEARN_DATASET/SCIKIT_LEARN_DATASET_test_.py
+++ GENERATORS/SAMPLE_DATASETS/SCIKIT_LEARN_DATASET/SCIKIT_LEARN_DATASET_test_.py
@@ -1,21 +1,20 @@
-# from flojoy import DataFrame
-#
-#
-#
-# # Tests that the function returns the expected DataFrame when called with the default dataset_key parameter
-# def test_default_dataset_key(mock_flojoy_decorator):
-#     import SCIKIT_LEARN_DATASET
-#
-#     result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET()
-#     assert isinstance(result, DataFrame)
-#     assert result.m.equals(data("iris"))
-#
-#
-# # Tests that the function returns the expected DataFrame when called with the 'cars' dataset_key parameter
-# def test_cars_dataset_key(mock_flojoy_decorator):
-#     import SCIKIT_LEARN_DATASET
-#
-#     dataset_key = "cars"
-#     result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_key=dataset_key)
-#     assert isinstance(result, DataFrame)
-#     assert result.m.equals(data(dataset_key))
+from flojoy import DataFrame
+from sklearn.datasets import load_diabetes, load_iris
+
+
+# Tests that the function returns the expected DataFrame when called with the default dataset_key parameter
+def test_load_iris(mock_flojoy_decorator):
+    import SCIKIT_LEARN_DATASET
+
+    result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_name="iris")
+    assert isinstance(result, DataFrame)
+    assert result.m.equals(load_iris(as_frame=True, return_X_y=True)[0])
+
+
+# Tests that the function returns the expected DataFrame when called with the 'cars' dataset_key parameter
+def test_load_diabetes(mock_flojoy_decorator):
+    import SCIKIT_LEARN_DATASET
+
+    result = SCIKIT_LEARN_DATASET.SCIKIT_LEARN_DATASET(dataset_name="diabetes")
+    assert isinstance(result, DataFrame)
+    assert result.m.equals(load_diabetes(as_frame=True, return_X_y=True)[0])
--- GENERATORS/SAMPLE_DATASETS/SCIKIT_LEARN_DATASET/example.md
+++ GENERATORS/SAMPLE_DATASETS/SCIKIT_LEARN_DATASET/example.md
@@ -1,9 +1,7 @@
-## The R_DATASET app
+## The SCIKIT_LEARN_DATASET app
 
 The workflow of this app is described below:
 
-[R_DATASET](https://github.com/flojoy-io/nodes/blob/main/GENERATORS/SAMPLE_DATASET/R_DATASET/R_DATASET.py) : This is a R Dataset node. It takes one parameter  `dataset_key`, the name of dataset to load from [`rdatasets`](https://github.com/vincentarelbundock/Rdatasets/tree/master/csv/datasets) package. In this case it is 'iris' which is default value of this parameter. It passing a `DataFrame` object of `DataContainer` class to the next node `Table`.
+[SCIKIT_LEARN_DATASET](https://github.com/flojoy-io/nodes/blob/main/GENERATORS/SAMPLE_DATASET/SCIKIT_LEARN_DATASET/SCIKIT_LEARN_DATASET.py) : This is a SCIKIT_LEARN_DATASET node. It takes one parameter `dataset_name`, the name of dataset to load from [`sklearn.datasets`](https://scikit-learn.org/stable/datasets/toy_dataset.html) package. In this case it is 'iris' which is default value of this parameter. It passing a `DataFrame` object of `DataContainer` class to the next node `Table`.
 
-[TABLE](https://github.com/flojoy-io/nodes/blob/main/VISUALIZERS/PLOTLY/TABLE/TABLE.py): This node creates a Plotly table visualization for a given input `DataFrame` object of `DatacContainer` class.
-
-[TERMINATE](https://github.com/flojoy-io/nodes/blob/main/LOGIC_GATES/TERMINATORS/END/END.py): This node terminating the current script run. The output of this node is same as its parent node.
+[TABLE](https://github.com/flojoy-io/nodes/blob/main/VISUALIZERS/PLOTLY/TABLE/TABLE.py): This node creates a Plotly table visualization for a given input `DataFrame` object of `DataContainer` class.
