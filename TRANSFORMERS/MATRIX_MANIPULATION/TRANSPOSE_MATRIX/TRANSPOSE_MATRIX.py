25cedc50dc4834f3ea21b8cf1e0a39da5158f31b
Jinwon Lee <jinwonlee0916@gmail.com>
2023-10-06 15:41:57 -0400

    TRANSPOSE_MATRIX

--- TRANSFORMERS/MATRIX_MANIPULATION/TRANSPOSE_MATRIX/TRANSPOSE_MATRIX.py
+++ TRANSFORMERS/MATRIX_MANIPULATION/TRANSPOSE_MATRIX/TRANSPOSE_MATRIX.py
@@ -1,10 +1,9 @@
 from numpy import transpose
 from flojoy import flojoy, Matrix
-from typing import Optional, Union, List, Tuple
 
 
 @flojoy
-def TRANSPOSE_MATRIX(default: Matrix, axis: Optional[Union[List[int], Tuple[int, int]]] ) -> Matrix:
+def TRANSPOSE_MATRIX(default: Matrix) -> Matrix:
     """The TRANSPOSE_MATRIX node takes an input matrix and transpose it along the chosen axis.
 
     Inputs
@@ -12,19 +11,10 @@ def TRANSPOSE_MATRIX(default: Matrix, axis: Optional[Union[List[int], Tuple[int,
     a : Matrix
         The input matrix to be transposed
 
-    Parameters
-    ----------
-    axis : int
-        Axis along which it is transposed. It takes a list of int where the length of a list is the
-        number of axis in an input matrix. It will return the list where the it transpose the
-        matrix based on the ith index of the list.
-
     Returns
     -------
     Matrix
         The matrix result from transposing.
     """
-    if len(axis) > len(default.m):
-        raise AssertionError("Invalid axis")
     
-    return Matrix(m=transpose(default.m, axis))
+    return Matrix(m=transpose(default.m, (1,0)))
