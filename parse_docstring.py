64c4ad41df93f827c3a190c658fb19aabb7cbf96
Joey Yu <joey@jyu.dev>
2023-10-17 17:01:20 -0400

    docstring.json -> block_data.json

--- parse_docstring.py
+++ parse_docstring.py
@@ -79,10 +79,11 @@ for root, dirs, files in os.walk(current_directory):
                                 }
                                 for rtn in parsed_docstring.many_returns
                             ],
+                            "code": code,
                         }
 
                         # Write the data to a JSON file in the same directory
-                        output_file_path = os.path.join(root, "docstring.json")
+                        output_file_path = os.path.join(root, "block_data.json")
                         with open(output_file_path, "w") as output_file:
                             json.dump(json_data, output_file, indent=2)
 
