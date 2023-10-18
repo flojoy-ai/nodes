ab8165d2af41d746c3591e1ca81ef84013a9488f
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-10-18 14:51:48 -0400

    flushed doc string

--- TRANSFORMERS/IMAGE_PROCESSING/IMAGE_RESIZE/IMAGE_RESIZE.py
+++ TRANSFORMERS/IMAGE_PROCESSING/IMAGE_RESIZE/IMAGE_RESIZE.py
@@ -1,4 +1,4 @@
-from flojoy import flojoy, Image, Greyscale
+from flojoy import flojoy, Image, Grayscale
 import cv2
 import numpy as np
 from typing import Literal
@@ -13,11 +13,42 @@ class options(Enum):
 
 @flojoy(deps={"opencv-python": "4.8.0.76"})
 def IMAGE_RESIZE(
-    default: Image | Greyscale,
-    fx: int = 2,
-    fy: int = 2,
-    interpolation: Literal["nearest", "linear", "cubic", "area", "Lanczos"] = "cubic",
-) -> Image | Greyscale:
+    default: Image | Grayscale,
+    fx: float = 2,
+    fy: float = 2,
+    interpolation: Literal["nearest", "linear", "cubic", "area", "Lanczos"] = "linear",
+) -> Image | Grayscale:
+    """The IMAGE_RESIZE nodes provides image rescaling on either an Image or Grayscale object. 
+    The method of rescaling can be adjusted, as well as the degree of scaling in each direction. 
+    There are a few methods by which one can scale an image:
+
+    - nearest       :       a nearest-neighbor interpolatino
+    - linear        :       a bilinear interpolation (default)
+    - area          :       resampling using pixel area relation. For 
+                            decimation, this is preferred to give Moiré 
+                            free results, but when zooming it is similar to the nearest method
+    - cubic         :       a bicubic interoplation over 4x4 neighborhood
+    - Lanczos       :       a Lanczos interpolation over 8x8 pixel neighborhood
+
+    See https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#cv2.resize for details.
+
+    Parameters
+    ------
+    default         :       Image or Grayscale
+        The image to be resized
+    fx              :       float
+        Degree of scaling in the x direction (can be noninteger)
+    fy              :       float
+        Degree of scaling in the y direction (can be noninteger)
+    interpolation   :       str
+        Method of interpolation to implement
+
+    Returns
+    -------
+    res             :       Image or Grayscale
+        The resized image
+
+    """
     try:
         interpolation = options[interpolation].value[0]
     except:
@@ -28,7 +59,7 @@ def IMAGE_RESIZE(
             img = cv2.merge([default.r, default.g, default.b, default.a])
         else:
             img = cv2.merge([default.r, default.g, default.b])
-    elif isinstance(default, Greyscale):
+    elif isinstance(default, Grayscale):
         img = cv2.merge([default.m, np.zeros_like(default.m), np.zeros_like(default.m)])
 
     #Images should be of shapes (NX, NY, 3/4)
@@ -40,4 +71,4 @@ def IMAGE_RESIZE(
         interpolation=interpolation
     )
     # raise ValueError(f"{img.shape},{res.shape}")
-    return Image(r=res[...,0], g=res[...,1], b=res[...,2], a=res[...,3] if default.a is not None else None) if isinstance(default, Image) else Greyscale(m=res[...,0])
+    return Image(r=res[...,0], g=res[...,1], b=res[...,2], a=res[...,3] if default.a is not None else None) if isinstance(default, Image) else Grayscale(m=res[...,0])
