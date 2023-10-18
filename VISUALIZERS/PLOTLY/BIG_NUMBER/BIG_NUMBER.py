55f54f116194a1629bdc0d10cea7090c0bc3c24c
JeffDotPng <zhangjeff2014@gmail.com>
2023-10-18 18:29:15 -0400

    add scientific notation to big number

--- VISUALIZERS/PLOTLY/BIG_NUMBER/BIG_NUMBER.py
+++ VISUALIZERS/PLOTLY/BIG_NUMBER/BIG_NUMBER.py
@@ -4,16 +4,11 @@ from flojoy import (
     Scalar,
     Vector,
 )
-from typing import cast
-import plotly.graph_objects as go
-
-# from nodes.VISUALIZERS.template import plot_layout
-
-# MEMORY_KEY = "BIG_NUMBER_MEMORY_KEY"
 
 
 @flojoy(node_type="BIG_NUMBER", forward_result=True)
-def BIG_NUMBER(default: OrderedPair | Scalar | Vector, ) -> Scalar:
+def BIG_NUMBER(default: OrderedPair | Scalar | Vector,
+               scientific_notation: bool = False) -> Scalar:
     """The BIG_NUMBER node generates a Plotly figure, displaying a big number with an optional prefix and suffix.
 
     Inputs
@@ -38,12 +33,6 @@ def BIG_NUMBER(default: OrderedPair | Scalar | Vector, ) -> Scalar:
         the DataContainer containing the Plotly big number visualization
     """
 
-    # job_id = default_params.job_id
-    # node_name = __name__.split(".")[-1]
-    # layout = plot_layout(title=title if title else node_name)
-    # fig = go.Figure(layout=layout)
-    #
-    # prev_num = cast(str, SmallMemory().read_memory(job_id, MEMORY_KEY))
     match default:
         case OrderedPair():
             return Scalar(default.y[-1])
@@ -54,25 +43,3 @@ def BIG_NUMBER(default: OrderedPair | Scalar | Vector, ) -> Scalar:
         case _:
             raise ValueError(
                 f"Invalid input type {type(default)} for node {node_name}")
-    #
-    # delta_val_format = ".1%" if relative_delta is True else ".1f"
-    # val_format = "%g" if scientific_notation is False else ".4e"
-    #
-    # fig.add_trace(
-    #     go.Indicator(
-    #         mode="number+delta",
-    #         value=big_num,
-    #         domain={"y": [0, 1], "x": [0, 1]},
-    #         number={"prefix": prefix, "suffix": suffix, "valueformat": val_format},
-    #         delta=None
-    #         if prev_num is None
-    #         else {
-    #             "reference": float(prev_num),
-    #             "relative": relative_delta,
-    #             "valueformat": delta_val_format,
-    #         },
-    #     )
-    # )
-    # SmallMemory().write_to_memory(job_id, MEMORY_KEY, str(float(big_num)))
-    #
-    # return Plotly(fig=fig)
