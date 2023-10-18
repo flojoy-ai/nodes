477b5e3551ba1c082ffd3b471bfff2db82c7a3c5
JeffDotPng <zhangjeff2014@gmail.com>
2023-10-18 18:21:40 -0400

    remove send to socket

--- LOGIC_GATES/TIMERS/TIMER/TIMER.py
+++ LOGIC_GATES/TIMERS/TIMER/TIMER.py
@@ -1,15 +1,11 @@
-from flojoy import flojoy, DataContainer, DefaultParams, send_to_socket
-from flojoy.utils import PlotlyJSONEncoder
+from flojoy import flojoy, DataContainer
 from flojoy.job_result_builder import JobResultBuilder
-import plotly.graph_objects as go
 import time
-import json
 from typing import Optional, cast
 
 
-@flojoy(inject_node_metadata=True)
+@flojoy()
 def TIMER(
-    default_params: DefaultParams,
     default: Optional[DataContainer] = None,
     sleep_time: float = 0,
 ) -> DataContainer:
@@ -21,8 +17,6 @@ def TIMER(
         number of seconds to sleep
     """
 
-    node_id = default_params.node_id
-    jobset_id = default_params.jobset_id
     remaining_time = sleep_time
     start_time = time.time()
     current_time = start_time
@@ -33,28 +27,6 @@ def TIMER(
     )
 
     while current_time - start_time < sleep_time:
-        fig = go.Figure(
-            data=go.Indicator(
-                mode="number",
-                value=int(remaining_time),
-                domain={"y": [0, 1], "x": [0, 1]},
-                delta=None,
-            )
-        )
-        send_to_socket(
-            json.dumps(
-                {
-                    "NODE_RESULTS": {
-                        "cmd": "TIMER",
-                        "id": node_id,
-                        "result": {"plotly_fig": fig},
-                    },
-                    "proceed_to_next": False,
-                    "jobsetId": jobset_id,
-                },
-                cls=PlotlyJSONEncoder,
-            ),
-        )
         sleep_interval = min(1, remaining_time)
         time.sleep(sleep_interval)
         remaining_time = sleep_time - (current_time - start_time)
