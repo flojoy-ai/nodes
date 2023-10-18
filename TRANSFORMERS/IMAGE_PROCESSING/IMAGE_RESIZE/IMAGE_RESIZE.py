d32c2927a4088fe6d6792f490ed88a3bacac29b8
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-10-18 14:10:08 -0400

    allow greyscale type

--- TRANSFORMERS/IMAGE_PROCESSING/IMAGE_RESIZE/IMAGE_RESIZE.py
+++ TRANSFORMERS/IMAGE_PROCESSING/IMAGE_RESIZE/IMAGE_RESIZE.py
@@ -1,4 +1,4 @@
-from flojoy import flojoy, Image
+from flojoy import flojoy, Image, Greyscale
 import cv2
 import numpy as np
 from typing import Literal
@@ -13,20 +13,24 @@ class options(Enum):
 
 @flojoy(deps={"opencv-python": "4.8.0.76"})
 def IMAGE_RESIZE(
-    default: Image,
+    default: Image | Greyscale,
     fx: int = 2,
     fy: int = 2,
     interpolation: Literal["nearest", "linear", "cubic", "area", "Lanczos"] = "cubic",
-) -> Image:
+) -> Image | Greyscale:
     try:
         interpolation = options[interpolation].value[0]
     except:
         interpolation = options[interpolation].value
-    if default.a is not None:
-        img = cv2.merge([default.r, default.g, default.b, default.a])
-    else:
-        img = cv2.merge([default.r, default.g, default.b])
-        
+
+    if isinstance(default, Image):
+        if default.a is not None:
+            img = cv2.merge([default.r, default.g, default.b, default.a])
+        else:
+            img = cv2.merge([default.r, default.g, default.b])
+    elif isinstance(default, Greyscale):
+        img = cv2.merge([default.m, np.zeros_like(default.m), np.zeros_like(default.m)])
+
     #Images should be of shapes (NX, NY, 3/4)
     res = cv2.resize(
         img, 
@@ -36,4 +40,4 @@ def IMAGE_RESIZE(
         interpolation=interpolation
     )
     # raise ValueError(f"{img.shape},{res.shape}")
-    return Image(r=res[...,0], g=res[...,1], b=res[...,2], a=res[...,3] if default.a is not None else None)
+    return Image(r=res[...,0], g=res[...,1], b=res[...,2], a=res[...,3] if default.a is not None else None) if isinstance(default, Image) else Greyscale(m=res[...,0])
