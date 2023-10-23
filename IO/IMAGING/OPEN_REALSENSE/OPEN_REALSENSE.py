1f98f612bf379600af2c341c70f5141a440fd3fb
JeffDotPng <zhangjeff2014@gmail.com>
2023-10-23 15:30:18 -0400

    use parameters for realsense

--- IO/IMAGING/OPEN_REALSENSE/OPEN_REALSENSE.py
+++ IO/IMAGING/OPEN_REALSENSE/OPEN_REALSENSE.py
@@ -1,6 +1,6 @@
 from flojoy import CameraDevice, flojoy, OrderedTriple, DataContainer
 from flojoy.connection_manager import DeviceConnectionManager
-from typing import Optional
+from typing import Literal, Optional
 
 import pyrealsense2 as rs
 
@@ -8,14 +8,21 @@ import pyrealsense2 as rs
 @flojoy(deps={"pyrealsense2": "2.54.2.5684"})
 def OPEN_REALSENSE(
         device: CameraDevice,
+        width: int = 640,
+        height: int = 480,
+        rs_format: Literal["z16", "xyz32f"] = 'z16',
+        framerate: int = 30,
         default: Optional[DataContainer] = None) -> Optional[DataContainer]:
     pipeline = rs.pipeline()
     config = rs.config()
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
 
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    match rs_format:
+        case "z16":
+            fmt = rs.format.z16
+        case "xyz32f":
+            fmt = rs.format.xyz32f
+
+    config.enable_stream(rs.stream.depth, width, height, fmt, framerate)
 
     pipeline.start(config)
 
--- IO/IMAGING/REALSENSE/REALSENSE.py
+++ IO/IMAGING/REALSENSE/REALSENSE.py
@@ -1,20 +1,30 @@
 import numpy as np
 import cv2
-from flojoy import CameraConnection, DataContainer, flojoy, OrderedTriple
+from flojoy import CameraConnection, DataContainer, flojoy, OrderedTriple, node_initialization, NodeInitContainer
 from typing import Optional
 import pyrealsense2 as rs
 
-decimate = rs.decimation_filter(4)
+# decimate = rs.decimation_filter(4)
+# spatial = rs.spatial_filter()
+# spatial.set_option(rs.option.filter_magnitude, 2)
+# spatial.set_option(rs.option.filter_smooth_alpha, 0.8)
+# spatial.set_option(rs.option.filter_smooth_delta, 10)
+# spatial.set_option(rs.option.holes_fill, 3)
+# temporal = rs.temporal_filter()
 
 
 @flojoy(inject_connection=True)
 def REALSENSE(connection: CameraConnection,
+              init_container: NodeInitContainer,
               default: Optional[DataContainer] = None) -> OrderedTriple:
     pipeline = connection.get_handle()
+    filters = init_container.get()
 
     frames = pipeline.wait_for_frames()
     depth = frames.get_depth_frame()
-    depth = decimate.process(depth)
+    if filters is not None:
+        for filter in filters:
+            depth = filter.process(depth)
 
     depth_sensor = pipeline.get_active_profile().get_device(
     ).first_depth_sensor()
@@ -36,3 +46,30 @@ def REALSENSE(connection: CameraConnection,
                          y=yy.reshape(-1),
                          z=zz.reshape(-1),
                          extra={"colors": colors})
+
+
+@node_initialization(for_node=REALSENSE)
+def init_filters(decimate_filter: bool = True,
+                 decimate_size: int = 4,
+                 temporal_filter: bool = True,
+                 spatial_filter: bool = True,
+                 spatial_filter_magnitude: int = 2,
+                 spatial_filter_smooth_alpha: float = 0.8,
+                 spatial_filter_smooth_delta: int = 20):
+    filters = []
+    if decimate_filter:
+        filters.append(rs.decimation_filter(decimate_size))
+    if spatial_filter:
+        spatial = rs.spatial_filter()
+        spatial.set_option(rs.option.filter_magnitude,
+                           spatial_filter_magnitude)
+        spatial.set_option(rs.option.filter_smooth_alpha,
+                           spatial_filter_smooth_alpha)
+        spatial.set_option(rs.option.filter_smooth_delta,
+                           spatial_filter_smooth_delta)
+        spatial.set_option(rs.option.holes_fill, 3)
+        filters.append(spatial)
+    if temporal_filter:
+        filters.append(rs.temporal_filter())
+
+    return filters
